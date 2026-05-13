b659327a2d16bbe770593e77e71b2329b974fa9e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-13 21:38:04 +0530

    feat: initiate task entity

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/entities/TaskEntity.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/entities/TaskEntity.java
@@ -1,4 +1,28 @@
 package lk.spring_security.stateless_jwt.infrastructure.task.persistence.entities;
 
-public class TaskEntities {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lk.spring_security.stateless_jwt.infrastructure.user.persistence.entities.UserEntity;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskId;
+    @NotBlank(message = "Title cannot be empty")
+    private String taskTitle;
+    @NotBlank(message = "Description cannot be empty")
+    private String taskDescription;
+
+    private Boolean completed = false ;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity user;
 }
