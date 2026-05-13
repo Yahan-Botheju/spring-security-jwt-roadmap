b2d314a97f265e40fec87d679cf0630a79a5c6b2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-13 21:19:55 +0530

    feat: initiate user domain model

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/domain/models/User.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/domain/models/User.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateless_jwt.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private Long userId;
+    private String email;
+    private String password;
+    private Role role;
 }
