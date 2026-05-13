b74906e3a620d311e536cfdc529f40edf5ef1640
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-13 21:22:58 +0530

    feat: implement user jpa repo

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/user/persistence/jpa/JpaUserRepository.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/user/persistence/jpa/JpaUserRepository.java
@@ -1,6 +1,6 @@
-package lk.spring_security.stateless_jwt.infrastructure.persistence.jpa;
+package lk.spring_security.stateless_jwt.infrastructure.user.persistence.jpa;
 
-import lk.spring_security.stateless_jwt.infrastructure.persistence.entities.UserEntity;
+import lk.spring_security.stateless_jwt.infrastructure.user.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
