13d65b4de50e18ba8dc5fbef3aa0f80b6e7a5eed
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-13 21:25:44 +0530

    feat: initiate jpa task repo

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/jpa/JpaTaskRepository.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/jpa/JpaTaskRepository.java
@@ -1,4 +1,7 @@
 package lk.spring_security.stateless_jwt.infrastructure.task.persistence.jpa;
 
-public interface JpaTaskRepository {
+import lk.spring_security.stateless_jwt.infrastructure.task.persistence.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaTaskRepository extends JpaRepository<TaskEntity,Long> {
 }
