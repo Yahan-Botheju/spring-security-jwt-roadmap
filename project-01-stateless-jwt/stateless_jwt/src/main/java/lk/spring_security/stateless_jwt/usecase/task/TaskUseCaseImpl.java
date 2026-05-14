bc8f7086eb2c5d352cac97243498d5bb39486697
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-14 10:34:09 +0530

    feat: inject task domain repo

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.spring_security.stateless_jwt.usecase.task;
 
-public class TaskUseCase {
+import lk.spring_security.stateless_jwt.domain.repositories.TaskRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TaskUseCaseImpl implements TaskUseCase {
+
+    //inject task domain repo
+    private final TaskRepository taskRepository;
 }
