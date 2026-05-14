831e66f529162447626ae933553b62063c17b8a6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-14 08:54:18 +0530

    feat: initiate permissions type according to user roles

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/domain/models/User.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/domain/models/User.java
@@ -1,10 +1,18 @@
 package lk.spring_security.stateless_jwt.domain.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -12,4 +20,11 @@ public class User {
     private String email;
     private String password;
     private Role role;
+
+    //initiate permissions according roles of user
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+
 }
