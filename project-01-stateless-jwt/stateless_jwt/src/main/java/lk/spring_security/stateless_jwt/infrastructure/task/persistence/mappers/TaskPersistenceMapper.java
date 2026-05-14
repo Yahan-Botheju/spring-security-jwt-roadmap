31dd35d3cec3530e21d295abfadcdbce733ea343
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-14 10:41:39 +0530

    feat: initiate task persistence mapper

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateless_jwt.infrastructure.task.persistence.mappers;
 
+import lk.spring_security.stateless_jwt.domain.models.Task;
+import lk.spring_security.stateless_jwt.infrastructure.task.persistence.entities.TaskEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface TaskPersistenceMapper {
+
+    //domain model to entity
+    TaskEntity toEntity(Task task);
+
+    //entity to domain mode
+    Task toDomainModel(TaskEntity taskEntity);
 }
