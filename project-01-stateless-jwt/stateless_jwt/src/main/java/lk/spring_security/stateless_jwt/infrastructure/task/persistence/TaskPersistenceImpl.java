053ac07128c4c78d41bab1b91669c19a75a9b4ff
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-14 10:37:41 +0530

    feat: inject both jpa task repo and task persistence mapper

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/TaskPersistenceImpl.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/TaskPersistenceImpl.java
@@ -1,4 +1,16 @@
 package lk.spring_security.stateless_jwt.infrastructure.task.persistence;
 
-public class TaskPersistenceImpl {
+import lk.spring_security.stateless_jwt.domain.repositories.TaskRepository;
+import lk.spring_security.stateless_jwt.infrastructure.task.persistence.jpa.JpaTaskRepository;
+import lk.spring_security.stateless_jwt.infrastructure.task.persistence.mappers.TaskPersistenceMapper;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TaskPersistenceImpl implements TaskRepository {
+
+    //inject task jpa repo
+    private final JpaTaskRepository jpaTaskRepository;
+
+    //inject task persistence mapper
+    private final TaskPersistenceMapper taskPersistenceMapper;
 }
