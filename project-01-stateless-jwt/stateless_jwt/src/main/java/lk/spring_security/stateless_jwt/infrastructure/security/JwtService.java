201f4e4088a078cef03dd2d8699665264e370b67
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-15 19:27:32 +0530

    feat: create helper method/ string key turn into java secret key object

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/security/JwtService.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/security/JwtService.java
@@ -1,8 +1,13 @@
 package lk.spring_security.stateless_jwt.infrastructure.security;
 
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
 
 @Service
 public class JwtService {
@@ -10,6 +15,18 @@ public class JwtService {
     //create secret
     @Value("${application.security.jwt.secret-key}")
     private  String SECRET_KEY;
+
+
+    /* ----- HELPER METHODS ----- */
+
+    //turn KEY(String) to Java secretKey object
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    /* ----- PUBLIC METHODS ----- */
 
 
 }
