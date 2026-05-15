66dccd749ae4d85725881c9991193d81cae84bf2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-15 19:07:38 +0530

    feat: create secret

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/security/JwtService.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/security/JwtService.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateless_jwt.infrastructure.security;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+
+    //create secret
+    @Value("${application.security.jwt.secret-key}")
+    private  String SECRET_KEY;
+
+
 }
