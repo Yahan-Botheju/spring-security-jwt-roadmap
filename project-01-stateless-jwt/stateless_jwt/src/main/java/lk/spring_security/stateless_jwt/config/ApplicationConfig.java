de61327b4fb4180322f8411756c23e2297b4f503
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-17 13:19:19 +0530

    feat: inject user repo

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
@@ -1,4 +1,13 @@
 package lk.spring_security.stateless_jwt.config;
 
+import lk.spring_security.stateless_jwt.domain.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    //inject user repo
+    private final UserRepository userRepository;
 }
