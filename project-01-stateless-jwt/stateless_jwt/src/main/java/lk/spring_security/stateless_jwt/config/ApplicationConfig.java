9717e19865e2e18a2ecefbeed1b0bb493550cde5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-17 13:35:46 +0530

    feat: check user availability from db via spring security

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
@@ -2,7 +2,10 @@ package lk.spring_security.stateless_jwt.config;
 
 import lk.spring_security.stateless_jwt.domain.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
@@ -10,4 +13,11 @@ public class ApplicationConfig {
 
     //inject user repo
     private final UserRepository userRepository;
+
+    //find user in db using Spring security
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.userFindByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email" + username));
+    }
 }
