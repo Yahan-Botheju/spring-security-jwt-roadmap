fcdc3539c30606c3083a43d873922c45e886ee8e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-17 20:26:35 +0530

    feat: inject required spring classes and auth provider

--- project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/SecurityConfiguration.java
+++ project-01-stateless-jwt/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/SecurityConfiguration.java
@@ -1,4 +1,18 @@
 package lk.spring_security.stateless_jwt.config;
 
+import lk.spring_security.stateless_jwt.infrastructure.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    //inject jwt auth filter
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    //inject auth provider
+    private final AuthenticationProvider authenticationProvider;
 }
