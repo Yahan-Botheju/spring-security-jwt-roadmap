c77b292af3516b088c3d4af3de06b37e64dd51c6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-20 22:16:07 +0530

    feat: check user availability

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/auth/AuthUseCaseImpl.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/auth/AuthUseCaseImpl.java
@@ -6,7 +6,9 @@ import lk.spring_security.stateless_jwt.web.auth.AuthRequest;
 import lk.spring_security.stateless_jwt.web.auth.AuthResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class AuthUseCaseImpl implements AuthUseCase{
@@ -19,7 +21,12 @@ public class AuthUseCaseImpl implements AuthUseCase{
 
     //register new user
     @Override
+    @Transactional
     public AuthResponse register(AuthRequest authRequest){
 
+        //check user by email
+        if(userRepository.userFindByEmail(authRequest.getEmail()).isPresent()){
+            throw new UsernameNotFoundException("Email already exists");
+        }
     }
 }
