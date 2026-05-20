da79aa0dcca08fa21b1ca2c127ee31137a8f4a21
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-20 21:49:23 +0530

    feat: inject required classes

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/auth/AuthUseCaseImpl.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/auth/AuthUseCaseImpl.java
@@ -1,4 +1,18 @@
 package lk.spring_security.stateless_jwt.usecase.auth;
 
-public class AuthUseCaseImpl {
+import lk.spring_security.stateless_jwt.domain.repositories.UserRepository;
+import lk.spring_security.stateless_jwt.infrastructure.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+public class AuthUseCaseImpl implements AuthUseCase{
+
+    //inject required classes and spring classes
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
 }
