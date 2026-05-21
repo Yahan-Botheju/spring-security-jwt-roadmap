40aec15e9c041503b680bae9893c43d5fdcbf08c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-21 11:06:04 +0530

    feat: user login route

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/auth/AuthController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/auth/AuthController.java
@@ -3,6 +3,7 @@ package lk.spring_security.stateless_jwt.web.auth;
 import lk.spring_security.stateless_jwt.usecase.auth.AuthUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,18 @@ public class AuthController {
     private final AuthUseCase authUseCase;
 
     //register new user
+    @PostMapping("/register")
     public ResponseEntity<AuthResponseDTO> register(
             @RequestBody AuthRequestDTO authRequestDTO
     ){
         return  ResponseEntity.ok(authUseCase.register(authRequestDTO));
     }
 
-
+    //user login
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(
+            @RequestBody AuthRequestDTO authRequestDTO
+    ){
+        return  ResponseEntity.ok(authUseCase.login(authRequestDTO));
+    }
 }
