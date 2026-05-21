07cd92be8204c5c1828fd32eae878e2094e5d244
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-21 11:04:11 +0530

    feat: register new user

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/auth/AuthController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/auth/AuthController.java
@@ -2,6 +2,8 @@ package lk.spring_security.stateless_jwt.web.auth;
 
 import lk.spring_security.stateless_jwt.usecase.auth.AuthUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,13 @@ public class AuthController {
 
     //inject auth usecase
     private final AuthUseCase authUseCase;
+
+    //register new user
+    public ResponseEntity<AuthResponseDTO> register(
+            @RequestBody AuthRequestDTO authRequestDTO
+    ){
+        return  ResponseEntity.ok(authUseCase.register(authRequestDTO));
+    }
+
+
 }
