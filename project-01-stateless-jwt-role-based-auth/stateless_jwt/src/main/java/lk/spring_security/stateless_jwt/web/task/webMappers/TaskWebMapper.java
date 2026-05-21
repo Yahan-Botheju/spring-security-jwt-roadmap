67375a056541dd54009b264828253274a71f9130
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-21 20:02:14 +0530

    feat: create task web mapper

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
@@ -1,4 +1,16 @@
 package lk.spring_security.stateless_jwt.web.task.webMappers;
 
+import lk.spring_security.stateless_jwt.domain.models.Task;
+import lk.spring_security.stateless_jwt.web.task.DTOs.TaskRequestDTO;
+import lk.spring_security.stateless_jwt.web.task.DTOs.TaskResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface TaskWebMapper {
+
+    //dto to domain model
+    Task toDomainModel(TaskRequestDTO taskRequestDTO);
+
+    //domain model to dto
+    TaskResponseDTO toResponseDTO(Task task);
 }
