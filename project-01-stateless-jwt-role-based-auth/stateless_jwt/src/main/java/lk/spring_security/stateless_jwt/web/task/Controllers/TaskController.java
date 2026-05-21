cb34d7b7af5ae925bc86d541a1cc3e595020d6b2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-21 19:42:38 +0530

    feat: inject task web mapper

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/Controllers/TaskController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/Controllers/TaskController.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateless_jwt.web.task.Controllers;
 
+import lk.spring_security.stateless_jwt.web.task.webMappers.TaskWebMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    //inject task web mapper
+    private final TaskWebMapper taskWebMapper;
 }
--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
@@ -1,4 +1,4 @@
 package lk.spring_security.stateless_jwt.web.task.webMappers;
 
-public class TaskWebMapper {
+public interface TaskWebMapper {
 }
