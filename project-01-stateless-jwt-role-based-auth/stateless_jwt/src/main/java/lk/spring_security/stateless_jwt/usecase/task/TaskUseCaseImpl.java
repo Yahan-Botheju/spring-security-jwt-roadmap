5a0b7f4ababdabf979350e727888c39ef93b6989
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-21 20:01:47 +0530

    feat: get all tasks from domain repo then set to controller

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
@@ -1,11 +1,20 @@
 package lk.spring_security.stateless_jwt.usecase.task;
 
+import lk.spring_security.stateless_jwt.domain.models.Task;
 import lk.spring_security.stateless_jwt.domain.repositories.TaskRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TaskUseCaseImpl implements TaskUseCase {
 
     //inject task domain repo
     private final TaskRepository taskRepository;
+
+    //get all tasks
+    @Override
+    public List<Task> getAllTasks(){
+        return taskRepository.getAllTasks();
+    }
 }
