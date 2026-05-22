5432c4bbca19ff71e6dccf8e115d0e8734cc686a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-22 20:00:03 +0530

    feat: map user id when turn to entity

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
@@ -3,13 +3,16 @@ package lk.spring_security.stateless_jwt.infrastructure.task.persistence.mappers
 import lk.spring_security.stateless_jwt.domain.models.Task;
 import lk.spring_security.stateless_jwt.infrastructure.task.persistence.entities.TaskEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskPersistenceMapper {
 
     //domain model to entity
+    @Mapping(target = "user.userId", source = "userId")
     TaskEntity toEntity(Task task);
 
     //entity to domain mode
+    @Mapping(target = "userId", source = "user.userId")
     Task toDomainModel(TaskEntity taskEntity);
 }
