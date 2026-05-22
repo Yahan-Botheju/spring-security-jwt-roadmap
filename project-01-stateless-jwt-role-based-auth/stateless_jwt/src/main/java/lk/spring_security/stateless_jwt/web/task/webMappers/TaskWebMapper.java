faeabaf60a7befc243a01f341063ae38dcac8a11
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-22 20:01:35 +0530

    feat: map user id in response dto

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/task/webMappers/TaskWebMapper.java
@@ -4,6 +4,7 @@ import lk.spring_security.stateless_jwt.domain.models.Task;
 import lk.spring_security.stateless_jwt.web.task.DTOs.TaskRequestDTO;
 import lk.spring_security.stateless_jwt.web.task.DTOs.TaskResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskWebMapper {
@@ -12,5 +13,6 @@ public interface TaskWebMapper {
     Task toDomainModel(TaskRequestDTO taskRequestDTO);
 
     //domain model to dto
+    @Mapping(target = "userId", source = "userId")
     TaskResponseDTO toResponseDTO(Task task);
 }
