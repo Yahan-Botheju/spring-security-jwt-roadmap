837dd07788cf8dc53b90ba3beb1f949483b86147
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-22 21:22:29 +0530

    feat: nullable property ignore and implement task update mapper

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/infrastructure/task/persistence/mappers/TaskPersistenceMapper.java
@@ -2,8 +2,7 @@ package lk.spring_security.stateless_jwt.infrastructure.task.persistence.mappers
 
 import lk.spring_security.stateless_jwt.domain.models.Task;
 import lk.spring_security.stateless_jwt.infrastructure.task.persistence.entities.TaskEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface TaskPersistenceMapper {
@@ -15,4 +14,9 @@ public interface TaskPersistenceMapper {
     //entity to domain mode
     @Mapping(target = "userId", source = "user.userId")
     Task toDomainModel(TaskEntity taskEntity);
+
+    //update task
+    @Mapping(target = "user", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    TaskEntity updateTask(Task task, @MappingTarget TaskEntity taskEntity);
 }
