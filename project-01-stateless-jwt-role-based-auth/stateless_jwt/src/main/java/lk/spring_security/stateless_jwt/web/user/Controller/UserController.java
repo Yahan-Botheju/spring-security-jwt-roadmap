a48527f975ddc21688ae208d044f0b3259b594d7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-23 13:39:17 +0530

    feat: setup user profile route

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
@@ -1,8 +1,14 @@
 package lk.spring_security.stateless_jwt.web.user.Controller;
 
+import lk.spring_security.stateless_jwt.domain.models.User;
 import lk.spring_security.stateless_jwt.usecase.user.UserUseCase;
+import lk.spring_security.stateless_jwt.web.user.DTOs.UserResponseDTO;
 import lk.spring_security.stateless_jwt.web.user.webMappers.UserWebMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +22,16 @@ public class UserController {
 
     //inject user web mapper
     private final UserWebMapper userWebMapper;
+
+    //get user profile
+    @GetMapping("/profile")
+    public ResponseEntity<UserResponseDTO> getUserProfile(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String email = userDetails.getUsername();
+
+        User user = userUseCase.getUserProfile(email);
+        UserResponseDTO responseDTO = userWebMapper.toResponseDTO(user);
+        return ResponseEntity.ok(responseDTO);
+    }
 }
