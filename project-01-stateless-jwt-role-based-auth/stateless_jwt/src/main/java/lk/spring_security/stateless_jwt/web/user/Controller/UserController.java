b42fc76ac6aef9b226c454653cc97afc5e2b2034
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-23 13:18:01 +0530

    feat: inject user web mapper

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
@@ -1,6 +1,7 @@
 package lk.spring_security.stateless_jwt.web.user.Controller;
 
 import lk.spring_security.stateless_jwt.usecase.user.UserUseCase;
+import lk.spring_security.stateless_jwt.web.user.webMappers.UserWebMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +13,7 @@ public class UserController {
 
     //inject user usecase
     private final UserUseCase userUseCase;
+
+    //inject user web mapper
+    private final UserWebMapper userWebMapper;
 }
