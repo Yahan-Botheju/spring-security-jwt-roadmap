b5fe2a894384efa3a60a154bbf3ef98c97f2a90d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-23 13:38:24 +0530

    feat: initiate get specific user details

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/user/UserUseCase.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/user/UserUseCase.java
@@ -1,4 +1,9 @@
 package lk.spring_security.stateless_jwt.usecase.user;
 
+import lk.spring_security.stateless_jwt.domain.models.User;
+
 public interface UserUseCase {
+
+    //user profile
+    User getUserProfile(String email);
 }
