20d5cadcad5533ee3e00fc9c92f416d036197038
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-23 13:17:30 +0530

    feat: inject user usecase

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/web/user/Controller/UserController.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateless_jwt.web.user.Controller;
 
+import lk.spring_security.stateless_jwt.usecase.user.UserUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    //inject user usecase
+    private final UserUseCase userUseCase;
 }
