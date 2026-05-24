d37bc2853c52ef78c134fb437d879bb5372d85b4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-24 12:59:47 +0530

    fix: refactor and set to connnet with Custom user details service

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/config/ApplicationConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,13 +18,6 @@ public class ApplicationConfig {
 
     //inject user repo
     private final UserRepository userRepository;
-
-    //find user in db using Spring security
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> userRepository.userFindByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email" + username));
-    }
 
     //hashed password
     @Bean
@@ -43,8 +35,9 @@ public class ApplicationConfig {
 
     //create data access object auth provider
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
