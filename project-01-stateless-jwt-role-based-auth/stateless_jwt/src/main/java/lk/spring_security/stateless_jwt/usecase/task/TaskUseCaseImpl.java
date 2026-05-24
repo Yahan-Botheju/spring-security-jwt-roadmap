bbc16d3d857e65b060d9d682ea8cf64cc9c2bb73
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-24 13:47:40 +0530

    feat: make these classess independant/POJO

--- project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
+++ project-01-stateless-jwt-role-based-auth/stateless_jwt/src/main/java/lk/spring_security/stateless_jwt/usecase/task/TaskUseCaseImpl.java
@@ -2,15 +2,17 @@ package lk.spring_security.stateless_jwt.usecase.task;
 
 import lk.spring_security.stateless_jwt.domain.models.Task;
 import lk.spring_security.stateless_jwt.domain.repositories.TaskRepository;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 public class TaskUseCaseImpl implements TaskUseCase {
 
     //inject task domain repo
     private final TaskRepository taskRepository;
+
+    public TaskUseCaseImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     //get all tasks
     @Override
