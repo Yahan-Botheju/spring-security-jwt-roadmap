571ede7d7d4819bc3ffad06d5410fc6761d6ffd8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-26 01:01:52 +0530

    feat: add project-02 and configure core.longpaths

---
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/groovy/lk/spring_security/method_level_security_global_security_exceptions/MethodLevelSecurityGlobalSecurityExceptionsApplication.groovy
@@ -0,0 +1,13 @@
+package lk.spring_security.method_level_security_global_security_exceptions
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MethodLevelSecurityGlobalSecurityExceptionsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MethodLevelSecurityGlobalSecurityExceptionsApplication, args)
+    }
+
+}
---
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/test/groovy/lk/spring_security/method_level_security_global_security_exceptions/MethodLevelSecurityGlobalSecurityExceptionsApplicationTests.groovy
@@ -0,0 +1,13 @@
+package lk.spring_security.method_level_security_global_security_exceptions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MethodLevelSecurityGlobalSecurityExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
