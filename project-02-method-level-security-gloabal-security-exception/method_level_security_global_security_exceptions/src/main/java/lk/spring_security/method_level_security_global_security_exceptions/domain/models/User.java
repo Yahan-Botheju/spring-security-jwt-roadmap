ade44c324c7f82cea6fa1a4a1a695871343169c8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 12:55:34 +0530

    feat: initiate user domain model

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/models/User.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/models/User.java
@@ -1,4 +1,17 @@
 package lk.spring_security.method_level_security_global_security_exceptions.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    private Long userId;
+    private String email;
+    private String password;
+    private Role role;
 }
