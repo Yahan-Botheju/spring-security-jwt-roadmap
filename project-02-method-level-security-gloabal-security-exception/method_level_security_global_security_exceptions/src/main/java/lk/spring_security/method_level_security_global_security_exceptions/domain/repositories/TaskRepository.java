05d51e2b915dcb48f28697faeae9b688817f6a04
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 12:53:54 +0530

    feat: craete realationship with user entity

---
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/repositories/TaskRepository.java
@@ -0,0 +1,4 @@
+package lk.spring_security.method_level_security_global_security_exceptions.domain.repositories;
+
+public interface TaskRepository {
+}
--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
@@ -1,14 +1,11 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.annotation.Id;
 
@@ -31,4 +28,7 @@ public class TaskEntity {
 
     private Boolean completed = false;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
