0de99de98513d652df9d4dce48c4769dc0affa98
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:19:06 +0530

    feat: initiate user request dto fields

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/DTOs/UserRequestDTO.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/DTOs/UserRequestDTO.java
@@ -1,4 +1,19 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
 }
