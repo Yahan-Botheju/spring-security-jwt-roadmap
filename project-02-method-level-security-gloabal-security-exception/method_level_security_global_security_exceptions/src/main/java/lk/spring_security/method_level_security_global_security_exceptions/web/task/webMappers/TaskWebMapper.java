04afe01f89580beca94ef483027ac7a90a044b61
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:28:11 +0530

    feat: initiate task web mapper

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/webMappers/TaskWebMapper.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/webMappers/TaskWebMapper.java
@@ -1,4 +1,16 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.task.webMappers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
+import lk.spring_security.method_level_security_global_security_exceptions.web.task.DTOs.TaskRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.task.DTOs.TaskResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface TaskWebMapper {
+    //requestDTO to domain model
+    Task toDomainModel(TaskRequestDTO taskRequestDTO);
+
+    //domain model to responseDTO
+    TaskResponseDTO toResponseDTO(Task task);
+
 }
