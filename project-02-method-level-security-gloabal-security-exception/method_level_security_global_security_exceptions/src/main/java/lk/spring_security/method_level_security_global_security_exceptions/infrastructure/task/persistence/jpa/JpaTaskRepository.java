81291f8c65f4739a0b3616e12b22019da8043985
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 12:56:34 +0530

    feat: initiate jpa task repo

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/jpa/JpaTaskRepository.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/jpa/JpaTaskRepository.java
@@ -1,4 +1,7 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.jpa;
 
-public interface JpaTaskRepository {
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaTaskRepository extends JpaRepository<TaskEntity, Long> {
 }
