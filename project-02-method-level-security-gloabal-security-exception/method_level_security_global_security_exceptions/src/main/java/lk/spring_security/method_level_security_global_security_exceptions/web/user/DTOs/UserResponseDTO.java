3a29e8e04176a130d390742f89c6034333425d8f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:20:51 +0530

    feat: initiate user response dto fields

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/DTOs/UserResponseDTO.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/DTOs/UserResponseDTO.java
@@ -1,4 +1,15 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDTO {
+    private Long userId;
+    private String email;
+    private Role role;
 }
