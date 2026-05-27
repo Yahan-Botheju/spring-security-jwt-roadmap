9d18ab0c2a294e687a3481e15aaf18fe04e951ad
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:54:21 +0530

    feat: setup task persistence bean config

---
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/config/TaskPersistenceBeanConfig.java
@@ -0,0 +1,17 @@
+package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.config;
+
+import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.TaskRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.TaskPersistenceImpl;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.jpa.JpaTaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TaskPersistenceBeanConfig {
+    @Bean
+    public TaskRepository taskRepository(
+            JpaTaskRepository taskRepository
+    ) {
+        return new TaskPersistenceImpl(taskRepository);
+    }
+}
