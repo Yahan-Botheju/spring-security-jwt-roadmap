71164ca1342b0f9dd327e43f7e77307432fc71a6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 11:59:01 +0530

    feat: initiate task entity with fields

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
@@ -1,4 +1,34 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.SoftDelete;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "tasks")
+@SoftDelete(columnName = "is_deleted")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long takId;
+
+    @NotBlank(message = "Task Title cannot be empty")
+    private String tasKTitle;
+
+    @NotBlank(message = "Task Description cannot be empty")
+    private String taskDescription;
+
+    private Boolean completed = false;
+
 }
