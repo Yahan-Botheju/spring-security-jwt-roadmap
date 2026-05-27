37e2dd59dcbd95ff29cceaac0f080ddcb9a7cb13
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:23:22 +0530

    feat: initiate user web mapper

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/webMappers/UserWebMapper.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/webMappers/UserWebMapper.java
@@ -1,4 +1,15 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.user.webMappers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
+import lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs.UserRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs.UserResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserWebMapper {
+    //requestDTO to domain model
+    User toDomainModel(UserRequestDTO userRequestDTO);
+
+    //domain model to responseDTO
+    UserResponseDTO toResponseDTO(User user);
 }
