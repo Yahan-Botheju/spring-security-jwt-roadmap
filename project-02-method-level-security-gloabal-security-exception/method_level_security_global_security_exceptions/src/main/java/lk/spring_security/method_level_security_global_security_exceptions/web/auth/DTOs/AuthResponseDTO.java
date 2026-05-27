a61af21783fb35024d6c271533809d1863604801
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-27 13:10:40 +0530

    feat: initiate auth response dto fields

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/DTOs/AuthResponseDTO.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/DTOs/AuthResponseDTO.java
@@ -1,4 +1,12 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponseDTO {
+    private String token;
 }
