5f38e1e7f60cbe8e808fad6e20ac83fac1454660
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 20:33:02 +0530

    feat: create custom query for find user by username

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/user/persistence/jpa/JpaUserRepository.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/user/persistence/jpa/JpaUserRepository.java
@@ -3,5 +3,9 @@ package lk.spring_security.method_level_security_global_security_exceptions.infr
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserRepository  extends JpaRepository<UserEntity,Long> {
+
+    Optional<UserEntity> userFindByEmail(String email);
 }
