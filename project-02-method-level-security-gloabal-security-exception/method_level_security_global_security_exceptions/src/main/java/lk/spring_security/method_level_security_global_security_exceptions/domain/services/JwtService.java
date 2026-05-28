82688af1e10a4a177b26c4f50d8bd899cd92c67d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 20:56:02 +0530

    feat: initiate required methods for create token and validate its process

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/services/JwtService.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/services/JwtService.java
@@ -1,4 +1,12 @@
 package lk.spring_security.method_level_security_global_security_exceptions.domain.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Map;
+
 public interface JwtService {
+    String generateToken(String username);
+    String generateToken(Map<String, Object> extractClaims, UserDetails userDetails);
+    String extractUsername(String token);
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
