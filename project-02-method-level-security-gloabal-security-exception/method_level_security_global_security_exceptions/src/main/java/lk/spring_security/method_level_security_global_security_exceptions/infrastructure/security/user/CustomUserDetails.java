226f58dc96809fee0279c3d21c5f579abf7742ad
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 12:29:06 +0530

    feat: combine domain model

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetails.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetails.java
@@ -1,9 +1,14 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.user;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+
 
 }
