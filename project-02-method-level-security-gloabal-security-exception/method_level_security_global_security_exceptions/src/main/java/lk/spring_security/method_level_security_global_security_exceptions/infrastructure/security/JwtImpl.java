ea903ebdf75846abb1d45c31266f7ed0839eb499
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 21:00:15 +0530

    feat: intergrate with domain jwt service interface

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/JwtImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/JwtImpl.java
@@ -1,4 +1,6 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security;
 
-public class JwtImpl {
+import lk.spring_security.method_level_security_global_security_exceptions.domain.services.JwtService;
+
+public class JwtImpl implements JwtService {
 }
