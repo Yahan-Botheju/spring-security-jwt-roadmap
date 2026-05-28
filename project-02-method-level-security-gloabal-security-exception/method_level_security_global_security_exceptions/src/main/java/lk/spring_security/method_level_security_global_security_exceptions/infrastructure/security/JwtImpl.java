0f3e36bf524748c488432e709dc02470be39a678
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 21:22:53 +0530

    feat: create secret key turn into java secret key object method

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/JwtImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/JwtImpl.java
@@ -1,6 +1,12 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lk.spring_security.method_level_security_global_security_exceptions.domain.services.JwtService;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Base64;
 
 public class JwtImpl implements JwtService {
 
@@ -9,5 +15,13 @@ public class JwtImpl implements JwtService {
 
     public JwtImpl(String secretKey) {
         this.secret_key = secretKey;
+    }
+
+    /* ----- CREATE NEW TOKEN ----- */
+
+    //set key into java secret obj
+    private SecretKey createJavaSecretObject() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret_key);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
