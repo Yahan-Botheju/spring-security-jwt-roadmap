1ff1cff1db1a601fc9abc3df56ef9b90997c8afd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 20:01:41 +0530

    feat: implements with spring scurity adapter to connect with db

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
@@ -1,4 +1,6 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.user;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
 }
