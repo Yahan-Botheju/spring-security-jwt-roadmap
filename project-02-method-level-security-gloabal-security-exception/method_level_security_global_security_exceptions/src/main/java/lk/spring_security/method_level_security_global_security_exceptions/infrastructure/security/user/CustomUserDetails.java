5e125a7cfec040bae0e646f2934ff278fa867b62
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 12:27:51 +0530

    feat: combine with spring security to user

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetails.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetails.java
@@ -1,4 +1,9 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.user;
 
-public class CustomUserDetails {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
 }
