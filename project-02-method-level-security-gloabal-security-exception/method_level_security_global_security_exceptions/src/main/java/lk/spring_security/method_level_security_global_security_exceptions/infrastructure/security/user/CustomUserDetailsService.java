f69528c437ec913587db6ea61ca85253c2436a1e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 20:06:35 +0530

    feat: inject user repo for create db connection with spring security

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
@@ -1,6 +1,16 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.user;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class CustomUserDetailsService implements UserDetailsService {
+
+    //inject user repo (CONSTRUCTOR INJECTION)
+    private final UserRepository userRepository;
+
+    //(CONSTRUCTOR INJECTION)
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
