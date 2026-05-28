2db90e6a0ec72995864c079d2b79a9150c43921e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-28 20:44:27 +0530

    feat: create method that gets user details from db and set to Custom user details feilds

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/user/CustomUserDetailsService.java
@@ -1,7 +1,11 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.user;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.UserRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -13,4 +17,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    //check user through custom method and then set to custom user details
+    @Override
+    public UserDetails loadUserByUsername(
+            @NotNull String username
+    )throws UsernameNotFoundException {
+        User user = userRepository.userFindByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found " + " , " +username));
+
+        return new CustomUserDetails(user);
+    }
 }
