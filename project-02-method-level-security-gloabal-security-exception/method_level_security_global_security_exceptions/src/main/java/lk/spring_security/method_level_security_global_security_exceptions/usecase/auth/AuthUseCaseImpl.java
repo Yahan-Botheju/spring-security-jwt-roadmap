6ecc8d1bb3d5c5821a721e4f4be6744f5ca44305
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-29 20:58:28 +0530

    feat: implement register user method, then check user email availablity, then create user domain model

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCaseImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCaseImpl.java
@@ -1,8 +1,14 @@
 package lk.spring_security.method_level_security_global_security_exceptions.usecase.auth;
 
+import jakarta.transaction.Transactional;
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Role;
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.UserRepository;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.JwtImpl;
+import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthResponseDTO;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthUseCaseImpl implements AuthUseCase {
@@ -18,5 +24,24 @@ public class AuthUseCaseImpl implements AuthUseCase {
         this.passwordEncoder = passwordEncoder;
         this.jwtImpl = jwtImpl;
         this.authenticationManager = authenticationManager;
+    }
+
+    //register user
+    @Override
+    @Transactional
+    public AuthResponseDTO registerUser(
+            AuthRequestDTO authRequestDTO
+    ){
+        //check email availability
+        if(userRepository.userFindByEmail(authRequestDTO.getEmail()).isPresent()){
+            throw new IllegalArgumentException("email already exists");
+        }
+
+        //create domain model
+        User user = User.builder()
+                .email(authRequestDTO.getEmail())
+                .password(passwordEncoder.encode(authRequestDTO.getPassword()))
+                .role(Role.USER)
+                .build();
     }
 }
