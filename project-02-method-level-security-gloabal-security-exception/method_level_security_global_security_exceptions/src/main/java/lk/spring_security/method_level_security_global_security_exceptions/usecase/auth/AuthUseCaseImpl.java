0fd96fb56729fa2f8532d0e6ecd9107b5da52828
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-29 20:46:26 +0530

    feat: inject required classes via constructor injection

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCaseImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCaseImpl.java
@@ -1,4 +1,22 @@
 package lk.spring_security.method_level_security_global_security_exceptions.usecase.auth;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.UserRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.JwtImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class AuthUseCaseImpl implements AuthUseCase {
+
+    //inject required classes and spring classes via constructor injection
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtImpl jwtImpl;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthUseCaseImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtImpl jwtImpl, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtImpl = jwtImpl;
+        this.authenticationManager = authenticationManager;
+    }
 }
