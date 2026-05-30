b71ad9f9afc78bddff810076e0688594a1b4d160
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-30 12:25:21 +0530

    feat: config spring security to get user data from db using user details service and verify password

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/ApplicationConfig.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/ApplicationConfig.java
@@ -3,7 +3,10 @@ package lk.spring_security.method_level_security_global_security_exceptions.infr
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,5 +25,16 @@ public class ApplicationConfig {
                 AuthenticationConfiguration authConfiguration
     ) {
         return authConfiguration.getAuthenticationManager();
+    }
+
+    //config spring security to get user details from db and verify password
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            UserDetailsService userDetailsService
+    ) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
     }
 }
