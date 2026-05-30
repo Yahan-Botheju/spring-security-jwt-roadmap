83a92f6ae284f36710afd98ec48036705c13c3d3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-30 11:57:22 +0530

    feat: create hased password bean

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/ApplicationConfig.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/ApplicationConfig.java
@@ -1,4 +1,16 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ApplicationConfig {
+
+    //hashed password
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
