a0e3b350592637656c49bc18934cd4cab7de1017
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-30 13:29:28 +0530

    feat: inject auth usecase as constructor injection

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/controllers/AuthController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/controllers/AuthController.java
@@ -1,4 +1,18 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.auth.controllers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.usecase.auth.AuthUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/auth")
 public class AuthController {
+
+    //inject auth usecase
+    private final AuthUseCase authUseCase;
+
+    public AuthController(AuthUseCase authUseCase) {
+        this.authUseCase = authUseCase;
+    }
 }
