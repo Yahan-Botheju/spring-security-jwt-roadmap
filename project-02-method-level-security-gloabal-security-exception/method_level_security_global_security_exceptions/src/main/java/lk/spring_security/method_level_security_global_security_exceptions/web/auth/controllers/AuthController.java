feb5d1b0f0b7339692883f0deb785fdd6971ca16
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-30 13:31:29 +0530

    feat: create register user route

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/controllers/AuthController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/controllers/AuthController.java
@@ -1,7 +1,11 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.auth.controllers;
 
 import lk.spring_security.method_level_security_global_security_exceptions.usecase.auth.AuthUseCase;
-import lombok.RequiredArgsConstructor;
+import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,13 @@ public class AuthController {
 
     public AuthController(AuthUseCase authUseCase) {
         this.authUseCase = authUseCase;
+    }
+
+    //register new user
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(
+            @RequestBody AuthRequestDTO authRequestDTO
+    ){
+        return ResponseEntity.ok(authUseCase.registerUser(authRequestDTO));
     }
 }
