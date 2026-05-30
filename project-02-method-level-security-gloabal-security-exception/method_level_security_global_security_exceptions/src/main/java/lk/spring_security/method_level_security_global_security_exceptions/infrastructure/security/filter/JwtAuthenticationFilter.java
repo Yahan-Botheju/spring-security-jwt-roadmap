948360c0885e76df8d3909e400cfd8677deb3ff6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-30 13:25:07 +0530

    feat: allow user to access route and set filter to next route

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/filter/JwtAuthenticationFilter.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -64,7 +65,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         userDetails,null, userDetails.getAuthorities()
                 );
 
+                //allow authenticated user to access
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
+        //set request to next route
+        filterChain.doFilter(request, response);
     }
 }
