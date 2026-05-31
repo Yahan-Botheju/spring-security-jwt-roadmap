4acc4b21f6b12e349ed22ba9f43949ed9025b83a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:51:00 +0530

    feat: create get all task functionality

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/task/TaskUseCaseImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/task/TaskUseCaseImpl.java
@@ -1,6 +1,9 @@
 package lk.spring_security.method_level_security_global_security_exceptions.usecase.task;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
 import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.TaskRepository;
+
+import java.util.List;
 
 public class TaskUseCaseImpl implements TaskUseCase{
 
@@ -9,5 +12,10 @@ public class TaskUseCaseImpl implements TaskUseCase{
 
     public TaskUseCaseImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    //get all task
+    public List<Task> getAllTasks(){
+        return taskRepository.getAllTasks();
     }
 }
