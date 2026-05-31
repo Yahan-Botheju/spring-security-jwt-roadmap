b248eb65caa578302e2874c5d5bb97ad96d84c8d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:38:48 +0530

    feat: injection required classes

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/controllers/TaskController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/controllers/TaskController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.task.controllers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.usecase.task.TaskUseCase;
+import lk.spring_security.method_level_security_global_security_exceptions.web.task.webMappers.TaskWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/tasks")
 public class TaskController {
+
+    //inject required classes via constructor injection
+    private final TaskUseCase taskUseCase;
+    private final TaskWebMapper taskWebMapper;
+
+    public TaskController(
+            TaskUseCase taskUseCase,
+            TaskWebMapper taskWebMapper
+    ) {
+        this.taskUseCase = taskUseCase;
+        this.taskWebMapper = taskWebMapper;
+    }
 }
