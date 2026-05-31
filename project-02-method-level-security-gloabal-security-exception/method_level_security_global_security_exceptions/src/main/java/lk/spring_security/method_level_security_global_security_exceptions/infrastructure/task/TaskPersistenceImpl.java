54064f0a1bacdba6102835482bf6c5aa75756c60
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:34:40 +0530

    feat: inject persistence mapper

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/TaskPersistenceImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/TaskPersistenceImpl.java
@@ -1,14 +1,31 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
 import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.TaskRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity.TaskEntity;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.jpa.JpaTaskRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.taskPersistenceMapper.TaskPersistenceMapper;
+
+import java.util.List;
 
 public class TaskPersistenceImpl implements TaskRepository {
 
-    //inject jpaTaskRepo
+    //inject required classes
     private final JpaTaskRepository jpaTaskRepository;
+    private final TaskPersistenceMapper taskPersistenceMapper;
 
-    public TaskPersistenceImpl(JpaTaskRepository taskRepository) {
+
+    public TaskPersistenceImpl(
+            JpaTaskRepository taskRepository,
+            TaskPersistenceMapper taskPersistenceMapper
+    ) {
         this.jpaTaskRepository = taskRepository;
+        this.taskPersistenceMapper = taskPersistenceMapper;
+    }
+
+    //get all task
+    public List<Task> getAllTasks(){
+        List<TaskEntity> entityList = jpaTaskRepository.findAll();
+        return entityList.stream().map()
     }
 }
