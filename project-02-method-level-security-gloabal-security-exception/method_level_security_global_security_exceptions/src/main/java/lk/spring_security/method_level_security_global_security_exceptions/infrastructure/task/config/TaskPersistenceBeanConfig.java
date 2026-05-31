29965b0f82cdb994cc08bf645c8de5f78acd0b0f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:31:03 +0530

    feat: initiate task persistence mapper for injection

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/config/TaskPersistenceBeanConfig.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/config/TaskPersistenceBeanConfig.java
@@ -3,6 +3,7 @@ package lk.spring_security.method_level_security_global_security_exceptions.infr
 import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.TaskRepository;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.TaskPersistenceImpl;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.jpa.JpaTaskRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.taskPersistenceMapper.TaskPersistenceMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,9 @@ import org.springframework.context.annotation.Configuration;
 public class TaskPersistenceBeanConfig {
     @Bean
     public TaskRepository taskRepository(
-            JpaTaskRepository taskRepository
+            JpaTaskRepository taskRepository,
+            TaskPersistenceMapper taskPersistenceMapper
     ) {
-        return new TaskPersistenceImpl(taskRepository);
+        return new TaskPersistenceImpl(taskRepository, taskPersistenceMapper);
     }
 }
