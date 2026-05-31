4d81d38dd5f47e7a912a599625d68d2a0c8602cb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:51:29 +0530

    feat: setup get all task route

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/controllers/TaskController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/task/controllers/TaskController.java
@@ -1,9 +1,15 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.task.controllers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
 import lk.spring_security.method_level_security_global_security_exceptions.usecase.task.TaskUseCase;
+import lk.spring_security.method_level_security_global_security_exceptions.web.task.DTOs.TaskResponseDTO;
 import lk.spring_security.method_level_security_global_security_exceptions.web.task.webMappers.TaskWebMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2/tasks")
@@ -19,5 +25,13 @@ public class TaskController {
     ) {
         this.taskUseCase = taskUseCase;
         this.taskWebMapper = taskWebMapper;
+    }
+
+    //get all tasks
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks(){
+        List<Task> tasks = taskUseCase.getAllTasks();
+        List<TaskResponseDTO> responseDTO =tasks.stream().map(taskWebMapper::toResponseDTO).toList();
+        return ResponseEntity.ok(responseDTO);
     }
 }
