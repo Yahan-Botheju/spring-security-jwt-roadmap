f8e51691aa880a5992f8c9d775384ebede23b220
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 11:24:32 +0530

    feat: create update task mapper

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/taskPersistenceMapper/TaskPersistenceMapper.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/taskPersistenceMapper/TaskPersistenceMapper.java
@@ -3,6 +3,7 @@ package lk.spring_security.method_level_security_global_security_exceptions.infr
 import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity.TaskEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TaskPersistenceMapper {
@@ -12,4 +13,7 @@ public interface TaskPersistenceMapper {
 
     //entity to domain model
     Task toDomainModel(TaskEntity taskEntity);
+
+    //update task
+    TaskEntity updateTaskEntity(Task task, @MappingTarget  TaskEntity taskEntity);
 }
