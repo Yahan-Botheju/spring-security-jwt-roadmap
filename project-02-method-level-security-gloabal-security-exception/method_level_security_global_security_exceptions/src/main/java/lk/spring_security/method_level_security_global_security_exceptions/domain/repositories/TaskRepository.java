9be1ee011772b61ef5a081f2b1517f6fdec8c069
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-31 10:49:21 +0530

    feat: initiate get all task method

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/repositories/TaskRepository.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/repositories/TaskRepository.java
@@ -1,4 +1,10 @@
 package lk.spring_security.method_level_security_global_security_exceptions.domain.repositories;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.Task;
+
+import java.util.List;
+
 public interface TaskRepository {
+    //get all task
+    List<Task> getAllTasks();
 }
