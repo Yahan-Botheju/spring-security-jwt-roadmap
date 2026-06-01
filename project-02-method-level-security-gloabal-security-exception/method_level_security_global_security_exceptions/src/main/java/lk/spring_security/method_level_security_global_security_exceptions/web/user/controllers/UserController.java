4d91e96aacef5e605127987f0c53b081b3fff24a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 11:45:37 +0530

    feat: inject required classes

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/controllers/UserController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/controllers/UserController.java
@@ -1,4 +1,20 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.user.controllers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.usecase.user.UserUseCase;
+import lk.spring_security.method_level_security_global_security_exceptions.web.user.webMappers.UserWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/users")
 public class UserController {
+
+    //inject required classes
+    private final UserUseCase userUseCase;
+    private final UserWebMapper  userWebMapper;
+
+    public UserController(UserUseCase userUseCase, UserWebMapper userWebMapper) {
+        this.userUseCase = userUseCase;
+        this.userWebMapper = userWebMapper;
+    }
 }
