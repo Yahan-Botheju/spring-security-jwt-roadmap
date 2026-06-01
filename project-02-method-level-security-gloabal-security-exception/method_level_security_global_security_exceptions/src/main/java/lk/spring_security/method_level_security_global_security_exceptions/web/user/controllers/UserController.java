7df3f8af7bab7224cecc81dd297c5a5ca5bfdd0b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 12:01:45 +0530

    feat: setup update user route

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/controllers/UserController.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/user/controllers/UserController.java
@@ -1,9 +1,14 @@
 package lk.spring_security.method_level_security_global_security_exceptions.web.user.controllers;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.usecase.user.UserUseCase;
+import lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs.UserRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.user.DTOs.UserResponseDTO;
 import lk.spring_security.method_level_security_global_security_exceptions.web.user.webMappers.UserWebMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v2/users")
@@ -13,8 +18,24 @@ public class UserController {
     private final UserUseCase userUseCase;
     private final UserWebMapper  userWebMapper;
 
-    public UserController(UserUseCase userUseCase, UserWebMapper userWebMapper) {
+    public UserController(
+            UserUseCase userUseCase,
+            UserWebMapper userWebMapper
+    ) {
         this.userUseCase = userUseCase;
         this.userWebMapper = userWebMapper;
+    }
+
+    //update user profile
+    @PostMapping("/profile")
+    public ResponseEntity<UserResponseDTO> updateUser(
+            @RequestBody UserRequestDTO userRequestDTO,
+            @AuthenticationPrincipal UserDetails userDetails
+    ){
+        String userEmail = userDetails.getUsername();
+        UserResponseDTO responseDTO = userWebMapper.toResponseDTO(
+                userUseCase.updateUser(
+                        userEmail,userWebMapper.toDomainModel(userRequestDTO))  );
+        return ResponseEntity.ok(responseDTO);
     }
 }
