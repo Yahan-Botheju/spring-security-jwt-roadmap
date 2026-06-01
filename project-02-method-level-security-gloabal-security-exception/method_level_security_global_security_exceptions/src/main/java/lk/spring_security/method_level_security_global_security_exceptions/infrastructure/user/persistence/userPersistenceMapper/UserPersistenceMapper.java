b66233de2e12b73de7b56d212e51186a33e8deef
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 13:02:43 +0530

    feat: add null value igonre mapping

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/user/persistence/userPersistenceMapper/UserPersistenceMapper.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/user/persistence/userPersistenceMapper/UserPersistenceMapper.java
@@ -3,8 +3,10 @@ package lk.spring_security.method_level_security_global_security_exceptions.infr
 import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.task.persistence.entity.TaskEntity;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.entity.UserEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface UserPersistenceMapper {
@@ -15,5 +17,6 @@ public interface UserPersistenceMapper {
     User toDomainModel(UserEntity userEntity);
 
     //update user
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     UserEntity updateUser(User user, @MappingTarget UserEntity userEntity);
 }
