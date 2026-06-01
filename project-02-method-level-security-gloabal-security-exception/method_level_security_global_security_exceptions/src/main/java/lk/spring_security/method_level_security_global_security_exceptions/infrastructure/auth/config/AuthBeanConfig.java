2620e5646733d904fe3cb743796184d3a3b784f9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 13:07:55 +0530

    feat: config authUseCase bean

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/auth/config/AuthBeanConfig.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/auth/config/AuthBeanConfig.java
@@ -1,4 +1,23 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.auth.config;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.UserRepository;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.JwtImpl;
+import lk.spring_security.method_level_security_global_security_exceptions.usecase.auth.AuthUseCase;
+import lk.spring_security.method_level_security_global_security_exceptions.usecase.auth.AuthUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class AuthBeanConfig {
+    @Bean
+    public AuthUseCase authUseCase(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtImpl jwtImpl,
+            AuthenticationManager authenticationManager
+    ){
+        return new AuthUseCaseImpl(userRepository,passwordEncoder,jwtImpl,authenticationManager);
+    }
 }
