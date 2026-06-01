dbd53202090ecbfc96552efb6a2ec66b79a0bc07
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 12:00:19 +0530

    feat: initiate update user method

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/repositories/UserRepository.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/repositories/UserRepository.java
@@ -9,6 +9,9 @@ public interface UserRepository {
     //user find by email/username
     Optional<User> userFindByEmail(String email);
 
-    //save user
+    //save user (REGISTER USER)
     User saveUser(User user);
+
+    //update user
+    User updateUser(User user);
 }
