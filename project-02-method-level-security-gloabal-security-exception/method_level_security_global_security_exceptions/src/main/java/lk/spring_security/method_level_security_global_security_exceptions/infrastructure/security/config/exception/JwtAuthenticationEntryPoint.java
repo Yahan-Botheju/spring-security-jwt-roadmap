acbcbc12a18e2b384ddcd79bc67c9218a5f1f61e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 19:28:50 +0530

    feat: generate error handler config method

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/exception/JwtAuthenticationEntryPoint.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/exception/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,23 @@
 package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.config.exception;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException, ServletException {
+
+    }
 }
