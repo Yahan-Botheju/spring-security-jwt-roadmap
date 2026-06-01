121229d9a8ac242346935b27b54821718e541ab1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-01 19:27:17 +0530

    feat: initiate and implement with spring security error handler

---
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/security/config/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,8 @@
+package lk.spring_security.method_level_security_global_security_exceptions.infrastructure.security.config.exception;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+}
