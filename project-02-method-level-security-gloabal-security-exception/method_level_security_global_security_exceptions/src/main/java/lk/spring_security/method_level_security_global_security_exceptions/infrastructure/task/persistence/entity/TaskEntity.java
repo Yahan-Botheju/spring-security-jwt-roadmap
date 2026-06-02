aaa6afef023a14d8cfd7dd6474453cafe0d86265
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-02 11:15:54 +0530

    fix: type error of taskId

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/infrastructure/task/persistence/entity/TaskEntity.java
@@ -4,10 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SoftDelete;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "tasks")
@@ -15,10 +15,11 @@ import org.springframework.data.annotation.Id;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TaskEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long takId;
+    private Long taskId;
 
     @NotBlank(message = "Task Title cannot be empty")
     private String tasKTitle;
