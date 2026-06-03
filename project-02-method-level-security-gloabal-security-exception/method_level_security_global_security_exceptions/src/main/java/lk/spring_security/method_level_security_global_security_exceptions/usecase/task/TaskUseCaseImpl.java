3cbe70e8ff144f491ae9674c0f06b3db337cb0a8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-03 11:34:20 +0530

    fix: add override annotation

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/task/TaskUseCaseImpl.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/task/TaskUseCaseImpl.java
@@ -15,21 +15,25 @@ public class TaskUseCaseImpl implements TaskUseCase{
     }
 
     //get all task
+    @Override
     public List<Task> getAllTasks(){
         return taskRepository.getAllTasks();
     }
 
     //create task
+    @Override
     public Task createTask(Task task){
         return taskRepository.createTask(task);
     }
 
     //update task
+    @Override
     public Task updateTask(Long taskId, Task task){
         return taskRepository.updateTask(taskId, task);
     }
 
     //delete task
+    @Override
     public void  deleteTask(Long taskId){
         taskRepository.deleteTask(taskId);
     }
