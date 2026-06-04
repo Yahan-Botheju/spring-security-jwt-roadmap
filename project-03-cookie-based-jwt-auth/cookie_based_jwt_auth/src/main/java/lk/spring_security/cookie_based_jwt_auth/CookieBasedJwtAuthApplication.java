10e3a333ab1825b09f376d9368531508322f2f7b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-04 10:53:04 +0530

    add main class

---
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/CookieBasedJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package lk.spring_security.cookie_based_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieBasedJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieBasedJwtAuthApplication.class, args);
+    }
+
+}
