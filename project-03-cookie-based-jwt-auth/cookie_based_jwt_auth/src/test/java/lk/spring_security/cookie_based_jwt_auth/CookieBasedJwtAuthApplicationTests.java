459c6c5e677fb7aac88bc19885b8fc19d08b21ad
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-04 10:42:30 +0530

    add project 03

---
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/test/java/lk/spring_security/cookie_based_jwt_auth/CookieBasedJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.spring_security.cookie_based_jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieBasedJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
