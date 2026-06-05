0f5f11d736939c47c3c14ce230a11b2d37898217
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-05 18:51:53 +0530

    feat: create response dto

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/webMappers/AuthWebMapper.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/web/auth/webMappers/AuthWebMapper.java
@@ -2,6 +2,7 @@ package lk.spring_security.method_level_security_global_security_exceptions.web.
 
 import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthRequestDTO;
+import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +10,6 @@ public interface AuthWebMapper {
     //requestDTO to domain model
     User authToDomainModel(AuthRequestDTO authRequestDTO);
 
+    //domain model to responseDTO
+    AuthResponseDTO toAuthResponseDTO(User user);
 }
