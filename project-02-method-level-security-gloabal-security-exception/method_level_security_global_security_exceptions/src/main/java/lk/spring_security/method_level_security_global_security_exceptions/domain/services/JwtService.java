7e8d71a9ec73eb776a18e5924b1a8ce812610f29
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-05 20:19:49 +0530

    fix: remove all tightcappling interfaces usage and use User domain model for those places

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/services/JwtService.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/domain/services/JwtService.java
@@ -1,14 +1,14 @@
 package lk.spring_security.method_level_security_global_security_exceptions.domain.services;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 
 import java.util.Map;
 
 public interface JwtService {
-    String generateToken(UserDetails userDetails);
-    String generateToken(Map<String, Object> extractClaims, UserDetails userDetails);
+    String generateToken(User user);
+    String generateToken(Map<String, Object> extractClaims, User user);
     String extractUsername(String token);
-    boolean isTokenValid(String token, UserDetails userDetails);
+    boolean isTokenValid(String token, User user);
 
 
 }
