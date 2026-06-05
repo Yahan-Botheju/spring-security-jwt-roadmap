7c7ea6958ceafa1025ad092bacdf5dd761d88f76
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-05 13:09:17 +0530

    fix: change method to string

--- project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCase.java
+++ project-02-method-level-security-gloabal-security-exception/method_level_security_global_security_exceptions/src/main/java/lk/spring_security/method_level_security_global_security_exceptions/usecase/auth/AuthUseCase.java
@@ -1,12 +1,13 @@
 package lk.spring_security.method_level_security_global_security_exceptions.usecase.auth;
 
+import lk.spring_security.method_level_security_global_security_exceptions.domain.models.User;
 import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthRequestDTO;
 import lk.spring_security.method_level_security_global_security_exceptions.web.auth.DTOs.AuthResponseDTO;
 
 public interface AuthUseCase {
     //initiate register user
-    AuthResponseDTO registerUser(AuthRequestDTO authRequestDTO);
+    String registerUser(User user);
 
     //initiate login user
-    AuthResponseDTO loginUser(AuthRequestDTO authRequestDTO);
+    String loginUser(String email, String password);
 }
