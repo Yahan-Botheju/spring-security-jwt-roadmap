738a82593daef9d74cbe80b3ee11321fbed085ed
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-06 12:27:39 +0530

    feat: initiate user domain model fields

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/User.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/User.java
@@ -1,4 +1,15 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private Long userId;
+    private String email;
+    private String password;
+    private Role role;
 }
