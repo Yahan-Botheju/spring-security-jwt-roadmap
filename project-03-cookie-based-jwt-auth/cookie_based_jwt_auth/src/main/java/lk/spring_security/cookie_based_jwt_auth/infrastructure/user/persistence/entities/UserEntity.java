1aa0c4cb7d28265c11619a2298ae1644ef95c7fe
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-06 19:03:23 +0530

    feat: initate user entity

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/entities/UserEntity.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/entities/UserEntity.java
@@ -1,4 +1,30 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.entities;
 
+import jakarta.persistence.*;
+import lk.spring_security.cookie_based_jwt_auth.domain.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "users")
+@SoftDelete(columnName = "is_deleted")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
