8037f04fcb3179d58a4a1f0cf20ccc1731ff7903
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-06 12:26:36 +0530

    feat: initiate note domain model fields

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/Note.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/Note.java
@@ -1,4 +1,16 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Note {
+    private Long noteId;
+    private String title;
+    private String content;
+
+    private User user;
 }
