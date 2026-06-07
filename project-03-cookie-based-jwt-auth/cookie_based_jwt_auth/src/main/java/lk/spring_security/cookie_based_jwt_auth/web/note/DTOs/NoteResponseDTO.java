c36825772b805469743f6f5070139fe498b9759a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 12:38:04 +0530

    feat: intiate note response dto fields

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/DTOs/NoteResponseDTO.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/DTOs/NoteResponseDTO.java
@@ -1,4 +1,16 @@
 package lk.spring_security.cookie_based_jwt_auth.web.note.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NoteResponseDTO {
+    private Long noteId;
+    private String title;
+    private String content;
+
+    private Long userId;
 }
