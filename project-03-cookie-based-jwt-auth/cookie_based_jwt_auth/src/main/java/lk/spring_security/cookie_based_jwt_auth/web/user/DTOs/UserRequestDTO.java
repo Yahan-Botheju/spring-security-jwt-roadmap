3815a43ca724a7d12ce14a231cc7b10792a792d2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 12:05:25 +0530

    feat: initate user request dto fields

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/DTOs/UserRequestDTO.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/DTOs/UserRequestDTO.java
@@ -1,4 +1,22 @@
 package lk.spring_security.cookie_based_jwt_auth.web.user.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDTO {
+
+    @Email(message = "please, provide correct email")
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password should be at least 6 characters ")
+    private String password;
 }
