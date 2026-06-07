07ccfcd6cbb0cb239c5580b1b9bf205523726e59
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:46:20 +0530

    feat: initate note persistence bean config as note persistence is a bean

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/NotePersistenceBeanConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/NotePersistenceBeanConfig.java
@@ -1,4 +1,19 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._configs._persistenceBeanConfig;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.NoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.NoteRepositoryImpl;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.jpa.JpaNoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.persistenceMapper.NotePersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class NotePersistenceBeanConfig {
+    @Bean
+    public NoteRepository noteRepository(
+            JpaNoteRepository jpaNoteRepository,
+            NotePersistenceMapper notePersistenceMapper
+    ) {
+        return new NoteRepositoryImpl(jpaNoteRepository, notePersistenceMapper);
+    }
 }
