2c58f45c97ab6297a6179a8225b8c5019f756971
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:46:44 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/NoteRepositoryImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/NoteRepositoryImpl.java
@@ -1,4 +1,22 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence;
 
-public class NoteRepositoryImpl {
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.NoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.jpa.JpaNoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.persistenceMapper.NotePersistenceMapper;
+import lk.spring_security.method_level_security_global_security_exceptions.domain.repositories.TaskRepository;
+
+public class NoteRepositoryImpl implements NoteRepository {
+
+    //inject required classes
+    private final JpaNoteRepository jpaNoteRepository;
+    private final NotePersistenceMapper notePersistenceMapper;
+
+
+    public NoteRepositoryImpl(
+            JpaNoteRepository jpaNoteRepository,
+            NotePersistenceMapper notePersistenceMapper
+    ) {
+        this.jpaNoteRepository = jpaNoteRepository;
+        this.notePersistenceMapper = notePersistenceMapper;
+    }
 }
