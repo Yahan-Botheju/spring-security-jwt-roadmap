b2a9cf30526aa4430798952f119f0c70374c3c19
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:26:38 +0530

    feat: intiate user web mapper

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/webMapper/UserWebMapper.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/webMapper/UserWebMapper.java
@@ -1,4 +1,15 @@
 package lk.spring_security.cookie_based_jwt_auth.web.user.webMapper;
 
-public class UserWebMapper {
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+import lk.spring_security.cookie_based_jwt_auth.web.user.DTOs.UserRequestDTO;
+import lk.spring_security.cookie_based_jwt_auth.web.user.DTOs.UserResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserWebMapper {
+    //domain model to responseDTO
+    UserResponseDTO toResponseDTO(User user);
+
+    //requestDTO to domain model
+    User toDomainModel(UserRequestDTO userRequestDTO);
 }
