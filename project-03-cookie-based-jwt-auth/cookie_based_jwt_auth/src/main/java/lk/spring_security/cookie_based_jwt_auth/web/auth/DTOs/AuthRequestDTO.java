ea2237599dec1c32edbd3f68e6ade19c065be73e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:02:17 +0530

    feat: initiate auth request dto fields

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/DTOs/AuthRequestDTO.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/DTOs/AuthRequestDTO.java
@@ -1,4 +1,21 @@
 package lk.spring_security.cookie_based_jwt_auth.web.auth.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequestDTO {
+    @Email(message = "Please, enter valid email")
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "password cannot be empty")
+    @Size(min = 6, message = "password should have at least 6 characters")
+    private String password;
 }
