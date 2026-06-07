3a0ec9da76c47012b7b6582b6832249cadc0a2fa
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 14:27:40 +0530

    feat: initiate user usecase impl is a bean

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_usecaseBeanConfig/UserUseCaseBeanConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_usecaseBeanConfig/UserUseCaseBeanConfig.java
@@ -1,4 +1,17 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._configs._usecaseBeanConfig;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
+import lk.spring_security.cookie_based_jwt_auth.usecase.user.UserUseCase;
+import lk.spring_security.cookie_based_jwt_auth.usecase.user.UserUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserUseCaseBeanConfig {
+    @Bean
+    public UserUseCase userUseCase(
+            UserRepository userRepository
+    ) {
+        return new UserUseCaseImpl(userRepository);
+    }
 }
