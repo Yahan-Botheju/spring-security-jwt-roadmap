1fa14be9d74f5b962aac16b399c3119c00137c95
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:50:17 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence;
 
-public class UserRepositoryImpl {
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.persistenceMapper.UserPersistenceMapper;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.jpa.JpaUserRepository;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    //inject required classes
+    private UserPersistenceMapper userPersistenceMapper;
+    private JpaUserRepository jpaUserRepository;
+
+
+    public UserRepositoryImpl(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper) {
+        this.userPersistenceMapper = userPersistenceMapper;
+        this.jpaUserRepository = jpaUserRepository;
+    }
 }
