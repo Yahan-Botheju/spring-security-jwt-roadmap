1a8ae31a9b375187856434641b3825182dc1b036
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:49:55 +0530

    feat: initiate user bean config as user repo impl as bean

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/UserPersistenceBeanConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/UserPersistenceBeanConfig.java
@@ -1,4 +1,19 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._configs._persistenceBeanConfig;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.UserRepositoryImpl;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.persistenceMapper.UserPersistenceMapper;
+import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.jpa.JpaUserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserPersistenceBeanConfig {
+    @Bean
+    public UserRepository userRepository(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ) {
+        return new UserRepositoryImpl(jpaUserRepository, userPersistenceMapper);
+    }
 }
