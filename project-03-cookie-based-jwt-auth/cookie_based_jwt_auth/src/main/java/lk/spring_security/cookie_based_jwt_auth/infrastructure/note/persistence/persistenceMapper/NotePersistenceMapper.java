08e91fe336948ff555fb2a02835161bbb896db97
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-07 13:09:44 +0530

    feat: initiate note persistence mapper

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/persistenceMapper/NotePersistenceMapper.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/persistenceMapper/NotePersistenceMapper.java
@@ -1,4 +1,14 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.persistenceMapper;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.entities.NoteEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface NotePersistenceMapper {
+    //domain model to entity
+    NoteEntity toEntity(Note note);
+
+    //entity to domain model
+    Note toDomainModel(NoteEntity noteEntity);
 }
