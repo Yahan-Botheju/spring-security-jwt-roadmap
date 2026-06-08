f0b25e7cc6b0c448f35a8bbcc1214b613ef5fb68
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-08 12:35:24 +0530

    feat: create cookie name and cookie expire time

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/_shared/services/CookieService.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/_shared/services/CookieService.java
@@ -1,4 +1,20 @@
 package lk.spring_security.cookie_based_jwt_auth.web._shared.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CookieService {
+
+
+    /* ----- CREATE COOKIE ----- */
+
+    //create cookie name
+    @Value("${app.security.cookie-name}")
+    private String cookieName;
+
+    //initiate expire time
+    private static final int COOKIE_EXPIRY = 24 * 60 * 60;
+
+
 }
