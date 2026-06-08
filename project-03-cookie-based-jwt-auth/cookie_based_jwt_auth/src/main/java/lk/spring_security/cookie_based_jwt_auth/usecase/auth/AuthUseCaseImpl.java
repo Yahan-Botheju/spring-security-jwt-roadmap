cb5067c34a508347550236304a8cf09df56433bd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-08 10:29:15 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCaseImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCaseImpl.java
@@ -1,4 +1,26 @@
 package lk.spring_security.cookie_based_jwt_auth.usecase.auth;
 
-public class AuthUseCaseImpl {
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
+import lk.spring_security.cookie_based_jwt_auth.domain.services.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AuthUseCaseImpl implements AuthUseCase{
+
+    //inject required classes
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+
+    public AuthUseCaseImpl(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    //inject
 }
