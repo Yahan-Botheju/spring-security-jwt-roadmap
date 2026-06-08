805523fc337d0d9b214c8b7e98f0938bd4b9f3c8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-08 12:04:56 +0530

    fix: correct wrong classes import

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/UserPersistenceBeanConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_configs/_persistenceBeanConfig/UserPersistenceBeanConfig.java
@@ -2,8 +2,9 @@ package lk.spring_security.cookie_based_jwt_auth.infrastructure._configs._persis
 
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.UserRepositoryImpl;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.jpa.JpaUserRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.persistenceMapper.UserPersistenceMapper;
-import lk.spring_security.method_level_security_global_security_exceptions.infrastructure.user.persistence.jpa.JpaUserRepository;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
