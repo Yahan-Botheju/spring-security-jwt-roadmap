32e399173c2c39961adda9747dda0b85de4ccfd5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-09 20:51:53 +0530

    feat: initiate generate token methods

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/services/JwtService.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/services/JwtService.java
@@ -1,5 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.services;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+
+import java.util.Map;
+
 public interface JwtService {
+    String generateToken(User user);
+    String generateToken(Map<String,Object> extractClaims, User user);
     String extractUserName(String token);
 }
