f29964c554e8bb29ebffc05c305ab0b2a8a0f1ed
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-09 22:38:30 +0530

    generate required fields realated to spring security and set values to each fields from user domain model

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
@@ -1,7 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper;
 
 import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 public class CustomUserDetails  implements UserDetails {
 
@@ -10,5 +14,41 @@ public class CustomUserDetails  implements UserDetails {
 
     public CustomUserDetails(User user) {
         this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    @org.jspecify.annotations.Nullable
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
