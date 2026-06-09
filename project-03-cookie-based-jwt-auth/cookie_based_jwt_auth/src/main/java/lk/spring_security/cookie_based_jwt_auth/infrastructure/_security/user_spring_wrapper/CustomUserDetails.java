f9f4237921631ded0311add4436d9a12fec8ba60
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-09 22:32:44 +0530

    feat: inject user domain model

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
@@ -1,7 +1,14 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails  implements UserDetails {
 
+    //inject user domain model
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
 }
