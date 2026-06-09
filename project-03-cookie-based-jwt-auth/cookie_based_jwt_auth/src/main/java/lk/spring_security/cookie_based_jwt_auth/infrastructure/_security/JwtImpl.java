9b8f771afc1b160b1820e5348c8ec3d14b5d916b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-09 20:30:58 +0530

    feat: inject secret key

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/JwtImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/JwtImpl.java
@@ -1,6 +1,14 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security;
 
+import io.jsonwebtoken.Claims;
 import lk.spring_security.cookie_based_jwt_auth.domain.services.JwtService;
 
 public class JwtImpl implements JwtService {
+
+    //inject secret key
+    private final String SECRET_KEY;
+
+    public JwtImpl(String secretKey) {
+        this. SECRET_KEY = secretKey;
+    }
 }
