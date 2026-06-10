77c9d78f30cbd29199f498ce6cd477bf7a023142
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-10 20:01:35 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
@@ -1,6 +1,16 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class CustomUserDetailsService implements UserDetailsService {
+
+    //inject user repo
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
 }
