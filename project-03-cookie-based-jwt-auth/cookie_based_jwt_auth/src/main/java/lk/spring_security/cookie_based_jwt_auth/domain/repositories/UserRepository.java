07703b8ca00a8adefcd9ab4a3d0242761f8344bb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-10 20:25:48 +0530

    feat: initiate user fing by email method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/repositories/UserRepository.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.repositories;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+
+    //user find by email
+    Optional<User> userFindByEmail(String email);
 }
