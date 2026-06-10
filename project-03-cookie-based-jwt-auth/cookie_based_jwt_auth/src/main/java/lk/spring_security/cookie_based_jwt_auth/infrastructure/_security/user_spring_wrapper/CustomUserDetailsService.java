ee8eea2cbd5b11ad04632eda27351757a777489d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-10 20:04:20 +0530

    feat: generate user find method in custom user details service method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper;
 
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -12,5 +14,13 @@ public class CustomUserDetailsService implements UserDetailsService {
             UserRepository userRepository
     ) {
         this.userRepository = userRepository;
+    }
+
+    //find user by email from db
+    @Override
+    public UserDetails loadUserByUsername(
+            String email
+    ) throws UsernameNotFoundException {
+        return null;
     }
 }
