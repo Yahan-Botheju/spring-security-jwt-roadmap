849741f2744fa45c4e38287fc83297ce9aa57bfe
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-10 20:26:23 +0530

    feat: get user by email through db

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
@@ -1,8 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.jpa.JpaUserRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.persistenceMapper.UserPersistenceMapper;
+
+import java.util.Optional;
 
 
 public class UserRepositoryImpl implements UserRepository {
@@ -18,4 +21,11 @@ public class UserRepositoryImpl implements UserRepository {
         this.userPersistenceMapper = userPersistenceMapper;
         this.jpaUserRepository = jpaUserRepository;
     }
+
+    /* ----- __HELPER_METHODS__ ----- */
+    @Override
+    public Optional<User> userFindByEmail(String email){
+        return jpaUserRepository.findByEmail(email).map(userPersistenceMapper::toDomainModel);
+    }
+
 }
