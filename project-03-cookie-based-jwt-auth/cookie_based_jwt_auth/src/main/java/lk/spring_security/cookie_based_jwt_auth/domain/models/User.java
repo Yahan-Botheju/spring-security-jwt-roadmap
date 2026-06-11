fc2b6ac2b28fde3bd8edf3a7226e160242f203dd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 21:17:42 +0530

    feat: initiate Build annotation in user domain model

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/User.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/models/User.java
@@ -1,9 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
