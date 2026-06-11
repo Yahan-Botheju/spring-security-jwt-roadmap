3fe34ec11910f53b1e443d4c95d4884b5df1291b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 20:04:18 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/SecurityConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/SecurityConfig.java
@@ -1,4 +1,28 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.config;
 
+import lk.spring_security.cookie_based_jwt_auth.infrastructure._security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    //inject required classes as constructor injection
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private  final AuthenticationEntryPoint unauthorizedEntryPoint;
+
+    public SecurityConfig(
+            JwtAuthenticationFilter jwtAuthenticationFilter,
+            AuthenticationEntryPoint authenticationEntryPoint,
+            AuthenticationEntryPoint unauthorizedEntryPoint
+    ) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.unauthorizedEntryPoint = unauthorizedEntryPoint;
+    }
 }
