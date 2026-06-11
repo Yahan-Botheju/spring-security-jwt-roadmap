4b937b5a89ac0704cc8f7b6d4b8c7c6bf348d583
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 20:47:27 +0530

    fix: inject auth provider

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/SecurityConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.config
 import lk.spring_security.cookie_based_jwt_auth.infrastructure._security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,16 +19,16 @@ public class SecurityConfig {
 
     //inject required classes as constructor injection
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationProvider authenticationProvider;
     private  final AuthenticationEntryPoint unauthorizedEntryPoint;
 
     public SecurityConfig(
             JwtAuthenticationFilter jwtAuthenticationFilter,
-            AuthenticationEntryPoint authenticationEntryPoint,
+            AuthenticationProvider authenticationProvider,
             AuthenticationEntryPoint unauthorizedEntryPoint
     ) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationProvider = authenticationProvider;
         this.unauthorizedEntryPoint = unauthorizedEntryPoint;
     }
 
@@ -51,6 +52,9 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+
+                .authenticationProvider()
+                .addFilterBefore()
     }
 
 
