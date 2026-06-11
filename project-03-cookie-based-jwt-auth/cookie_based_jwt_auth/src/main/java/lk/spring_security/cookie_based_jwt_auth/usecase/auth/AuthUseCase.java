6c9ec27ec4d3f4c8032d4b166961e4ec58bced79
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 21:24:08 +0530

    feat: initiate register and login method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCase.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCase.java
@@ -1,4 +1,11 @@
 package lk.spring_security.cookie_based_jwt_auth.usecase.auth;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+
 public interface AuthUseCase {
+    //register new user
+    String registerUser(User user);
+
+    //login user
+    String loginUser(String email, String password);
 }
