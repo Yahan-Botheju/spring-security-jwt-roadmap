9b32007c28c46dc792e569e15fac71be456514ac
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 21:25:12 +0530

    feat: initiate register method, then check user availability and create user domain model if user is new

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCaseImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/auth/AuthUseCaseImpl.java
@@ -1,7 +1,10 @@
 package lk.spring_security.cookie_based_jwt_auth.usecase.auth;
 
+import jakarta.transaction.Transactional;
+import lk.spring_security.cookie_based_jwt_auth.domain.models.Role;
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
-import lk.spring_security.cookie_based_jwt_auth.domain.services.JwtService;
+import lk.spring_security.cookie_based_jwt_auth.domain.services.CookieService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthUseCaseImpl implements AuthUseCase{
@@ -9,18 +12,33 @@ public class AuthUseCaseImpl implements AuthUseCase{
     //inject required classes
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
+    private final CookieService cookieService;
 
 
     public AuthUseCaseImpl(
             UserRepository userRepository,
             PasswordEncoder passwordEncoder,
-            JwtService jwtService
+            CookieService cookieService
     ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
+        this.cookieService = cookieService;
     }
 
-    //inject
+    //register user
+    @Override
+    @Transactional
+    public String registerUser(User user){
+        //check user already exist in db
+        if(userRepository.userFindByEmail(user.getEmail()).isPresent()){
+            throw new IllegalArgumentException("User with email already exists");
+        }
+
+        //create user domain model
+        User createDomainModel = User.builder()
+                .email(user.getEmail())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .role(Role.USER)
+                .build();
+    }
 }
