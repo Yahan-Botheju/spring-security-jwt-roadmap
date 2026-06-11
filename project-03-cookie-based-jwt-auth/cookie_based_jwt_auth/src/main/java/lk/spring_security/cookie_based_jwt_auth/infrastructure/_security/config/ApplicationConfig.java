d3cc58b1d97d8e2b91324530763121a6727c0372
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-11 19:42:57 +0530

    feat: create password encoder method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/ApplicationConfig.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/_security/config/ApplicationConfig.java
@@ -1,4 +1,16 @@
 package lk.spring_security.cookie_based_jwt_auth.infrastructure._security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ApplicationConfig {
+
+    //password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
