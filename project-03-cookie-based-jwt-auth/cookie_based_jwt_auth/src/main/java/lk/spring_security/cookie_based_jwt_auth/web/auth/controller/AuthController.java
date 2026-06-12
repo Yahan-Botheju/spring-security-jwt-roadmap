5992a97eb402b79885f7dbf0ea19a58be4cd3246
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-12 21:21:13 +0530

    feat: initiate cookie for get response obj

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/controller/AuthController.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/controller/AuthController.java
@@ -1,8 +1,15 @@
 package lk.spring_security.cookie_based_jwt_auth.web.auth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lk.spring_security.cookie_based_jwt_auth.domain.services.CookieService;
 import lk.spring_security.cookie_based_jwt_auth.usecase.auth.AuthUseCase;
+import lk.spring_security.cookie_based_jwt_auth.web.auth.DTOs.AuthRequestDTO;
+import lk.spring_security.cookie_based_jwt_auth.web.auth.DTOs.AuthResponseDTO;
 import lk.spring_security.cookie_based_jwt_auth.web.auth.webMapper.AuthWebMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +21,22 @@ public class AuthController {
     private final AuthUseCase authUseCase;
     private final CookieService cookieService;
     private final AuthWebMapper authWebMapper;
+
+    public AuthController(
+            AuthUseCase authUseCase,
+            CookieService cookieService,
+            AuthWebMapper authWebMapper
+    ) {
+        this.authUseCase = authUseCase;
+        this.cookieService = cookieService;
+        this.authWebMapper = authWebMapper;
+    }
+
+    //register user
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> registerUser(
+            @Valid @RequestBody AuthRequestDTO authRequestDTO,
+            HttpServletRequest request //set cookie to get response
+            ){}
+
 }
