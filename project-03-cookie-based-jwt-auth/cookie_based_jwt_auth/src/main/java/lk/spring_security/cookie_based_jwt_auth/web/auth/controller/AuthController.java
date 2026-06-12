74089605d9ec8ff9cc4e5163135db683574e52fc
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-12 21:12:26 +0530

    feat: inject required classes

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/controller/AuthController.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/auth/controller/AuthController.java
@@ -1,4 +1,17 @@
 package lk.spring_security.cookie_based_jwt_auth.web.auth.controller;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.services.CookieService;
+import lk.spring_security.cookie_based_jwt_auth.usecase.auth.AuthUseCase;
+import lk.spring_security.cookie_based_jwt_auth.web.auth.webMapper.AuthWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    //inject required classes
+    private final AuthUseCase authUseCase;
+    private final CookieService cookieService;
+    private final AuthWebMapper authWebMapper;
 }
