69d542bb51b77a24eb8955a766f72fb3b48f32d0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-14 22:12:42 +0530

    feat: initiate get all note by user id method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/repositories/NoteRepository.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/domain/repositories/NoteRepository.java
@@ -1,4 +1,12 @@
 package lk.spring_security.cookie_based_jwt_auth.domain.repositories;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
+
+import java.util.List;
+
 public interface NoteRepository {
+
+    //get user all notes
+    List<Note> getAllNotesByUserId(Long userId);
+
 }
