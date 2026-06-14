0c36bda69095d9bd0d163915312af536caea7b1f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-14 22:23:39 +0530

    feat: map notes realted to userId

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/webMapper/NoteWebMapper.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/webMapper/NoteWebMapper.java
@@ -4,10 +4,12 @@ import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
 import lk.spring_security.cookie_based_jwt_auth.web.note.DTOs.NoteRequestDTO;
 import lk.spring_security.cookie_based_jwt_auth.web.note.DTOs.NoteResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface NoteWebMapper {
     //domain model to response
+    @Mapping(source = "user.userId", target = "userId")
     NoteResponseDTO toResponseDTO(Note note);
 
     //requestDTO to domain model
