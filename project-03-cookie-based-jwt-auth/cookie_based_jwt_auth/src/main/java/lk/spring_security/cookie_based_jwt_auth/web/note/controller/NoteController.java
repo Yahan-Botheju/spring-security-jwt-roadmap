ca0f89ffe877957a24457f61b776dd3dcdb93f1c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-15 11:35:45 +0530

    feat: setup create note route

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/controller/NoteController.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/note/controller/NoteController.java
@@ -1,16 +1,17 @@
 package lk.spring_security.cookie_based_jwt_auth.web.note.controller;
 
+import jakarta.validation.Valid;
 import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper.CustomUserDetails;
 import lk.spring_security.cookie_based_jwt_auth.usecase.note.NoteUseCase;
+import lk.spring_security.cookie_based_jwt_auth.web.note.DTOs.NoteRequestDTO;
 import lk.spring_security.cookie_based_jwt_auth.web.note.DTOs.NoteResponseDTO;
 import lk.spring_security.cookie_based_jwt_auth.web.note.webMapper.NoteWebMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,5 +42,19 @@ public class NoteController {
                 .map(noteWebMapper::toResponseDTO).collect(Collectors.toList());
 
         return ResponseEntity.ok(responseDTOS);
+    }
+
+    //create new note
+    @PostMapping
+    public ResponseEntity<NoteResponseDTO> createNote(
+            @AuthenticationPrincipal CustomUserDetails  customUserDetails,
+            @Valid @RequestBody NoteRequestDTO noteRequestDTO
+            ){
+        Long getUserId = customUserDetails.getUserId();
+        Note toDomainModel = noteWebMapper.toDomainModel(noteRequestDTO);
+
+        NoteResponseDTO responseDTO = noteWebMapper.toResponseDTO(noteUseCase.createNote(getUserId, toDomainModel));
+
+        return ResponseEntity.created(URI.create("/api/v1/notes")).body(responseDTO);
     }
 }
