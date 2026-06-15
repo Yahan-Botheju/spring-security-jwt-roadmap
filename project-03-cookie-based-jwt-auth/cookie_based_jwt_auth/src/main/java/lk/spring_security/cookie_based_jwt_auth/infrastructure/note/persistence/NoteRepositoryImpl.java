e9e9b1185ed7ca0734007c3822100e4a0214da15
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-15 11:33:56 +0530

    feat: save new note in db

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/NoteRepositoryImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/note/persistence/NoteRepositoryImpl.java
@@ -2,6 +2,7 @@ package lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence
 
 import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.NoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.entities.NoteEntity;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.jpa.JpaNoteRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.note.persistence.persistenceMapper.NotePersistenceMapper;
 
@@ -26,5 +27,15 @@ public class NoteRepositoryImpl implements NoteRepository {
     @Override
     public List<Note> getAllNotesByUserId(Long userId){
         return   jpaNoteRepository.findByUser_UserId(userId).stream().map(notePersistenceMapper::toDomainModel).toList();
+    }
+
+
+    //create note
+    @Override
+    public Note createNote(Note note){
+        NoteEntity toEntity = notePersistenceMapper.toEntity(note);
+        NoteEntity savedNote = jpaNoteRepository.save(toEntity);
+
+        return notePersistenceMapper.toDomainModel(savedNote);
     }
 }
