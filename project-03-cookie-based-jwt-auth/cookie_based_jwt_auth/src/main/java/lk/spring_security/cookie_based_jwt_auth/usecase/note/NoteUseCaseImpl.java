1655414c90a6c9bd98556aaf76e24c3b5309ba2b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-15 11:10:45 +0530

    feat: inject user domain repo

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/note/NoteUseCaseImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/note/NoteUseCaseImpl.java
@@ -2,6 +2,7 @@ package lk.spring_security.cookie_based_jwt_auth.usecase.note;
 
 import lk.spring_security.cookie_based_jwt_auth.domain.models.Note;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.NoteRepository;
+import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
 
 import java.util.List;
 
@@ -9,9 +10,12 @@ public class NoteUseCaseImpl implements  NoteUseCase {
 
     //inject required classes
     private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
 
-    public NoteUseCaseImpl(NoteRepository noteRepository) {
+
+    public NoteUseCaseImpl(NoteRepository noteRepository, UserRepository userRepository) {
         this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
     }
 
     //get user all notes
@@ -19,4 +23,6 @@ public class NoteUseCaseImpl implements  NoteUseCase {
     public List<Note> getAllNotesByUserId(Long userId){
         return noteRepository.getAllNotesByUserId(userId);
     }
+
+
 }
