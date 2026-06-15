4a83d57525641047a8cfdced1a5bbe9fde732efa
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-15 11:34:20 +0530

    feat:initiate find user in method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/infrastructure/user/persistence/UserRepositoryImpl.java
@@ -2,7 +2,6 @@ package lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence
 
 import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
-import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.entities.UserEntity;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.jpa.JpaUserRepository;
 import lk.spring_security.cookie_based_jwt_auth.infrastructure.user.persistence.persistenceMapper.UserPersistenceMapper;
 
@@ -28,6 +27,13 @@ public class UserRepositoryImpl implements UserRepository {
     public Optional<User> userFindByEmail(String email){
         return jpaUserRepository.findByEmail(email).map(userPersistenceMapper::toDomainModel);
     }
+
+    //user find by ID
+    @Override
+    public Optional<User> userFindById(Long userId){
+        return jpaUserRepository.findById(userId).map(userPersistenceMapper::toDomainModel);
+    }
+
 
     /* ----- __PUBLIC_METHODS__ ----- */
 
