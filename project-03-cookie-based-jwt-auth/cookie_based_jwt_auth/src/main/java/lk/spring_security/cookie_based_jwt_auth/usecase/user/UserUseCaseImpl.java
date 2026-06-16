e61a74a7891da6bca6e36ea6e9554659f835c52e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-16 20:11:36 +0530

    feat: create update user functionality

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/user/UserUseCaseImpl.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/user/UserUseCaseImpl.java
@@ -1,5 +1,6 @@
 package lk.spring_security.cookie_based_jwt_auth.usecase.user;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
 import lk.spring_security.cookie_based_jwt_auth.domain.repositories.UserRepository;
 
 public class UserUseCaseImpl implements UserUseCase{
@@ -10,4 +11,16 @@ public class UserUseCaseImpl implements UserUseCase{
     public UserUseCaseImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    //update user
+    @Override
+    public User updateUser(Long userId, User user) {
+        if(!userRepository.userFindById(userId).isPresent()){
+            throw new IllegalArgumentException("User does not exist");
+        }
+        user.setUserId(userId);
+        return userRepository.updateUser(user);
+    }
+
+
 }
