944dc97bf6a81f905306fecc4cbfb69b6ec0d21b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-16 20:59:49 +0530

    feat: setup user update route

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/controller/UserController.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/controller/UserController.java
@@ -1,9 +1,16 @@
 package lk.spring_security.cookie_based_jwt_auth.web.user.controller;
 
+import jakarta.validation.Valid;
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+import lk.spring_security.cookie_based_jwt_auth.infrastructure._security.user_spring_wrapper.CustomUserDetails;
 import lk.spring_security.cookie_based_jwt_auth.usecase.user.UserUseCase;
+import lk.spring_security.cookie_based_jwt_auth.web.user.DTOs.UserRequestDTO;
+import lk.spring_security.cookie_based_jwt_auth.web.user.DTOs.UserResponseDTO;
 import lk.spring_security.cookie_based_jwt_auth.web.user.webMapper.UserWebMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,5 +26,20 @@ public class UserController {
     ) {
         this.userUseCase = userUseCase;
         this.userWebMapper = userWebMapper;
+    }
+
+    //update user
+    @PutMapping("/profile")
+    public ResponseEntity<UserResponseDTO> updateUser(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @Valid @RequestBody UserRequestDTO userRequestDTO
+            ){
+        Long userId = customUserDetails.getUserId();
+
+        User toDomainModel = userWebMapper.toDomainModel(userRequestDTO);
+        User toUseCase = userUseCase.updateUser(userId, toDomainModel);
+        UserResponseDTO responseDTO = userWebMapper.toResponseDTO(toUseCase);
+
+        return ResponseEntity.ok(responseDTO);
     }
 }
