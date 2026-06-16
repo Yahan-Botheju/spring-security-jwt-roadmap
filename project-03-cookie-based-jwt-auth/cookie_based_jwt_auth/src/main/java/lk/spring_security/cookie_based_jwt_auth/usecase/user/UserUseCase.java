3333d385b365a193df18099e346426ad4b2d1663
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-16 20:10:53 +0530

    feat: initiate update method

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/user/UserUseCase.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/usecase/user/UserUseCase.java
@@ -1,4 +1,9 @@
 package lk.spring_security.cookie_based_jwt_auth.usecase.user;
 
+import lk.spring_security.cookie_based_jwt_auth.domain.models.User;
+
 public interface UserUseCase {
+
+    //update user
+    User updateUser(Long userId, User user);
 }
