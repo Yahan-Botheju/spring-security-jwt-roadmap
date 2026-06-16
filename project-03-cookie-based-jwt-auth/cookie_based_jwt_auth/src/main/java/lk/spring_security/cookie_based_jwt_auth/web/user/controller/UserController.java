d06ecc4897c71f65c2bbda8122624450453b5528
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-16 20:14:18 +0530

    feat: inject required dependancies

--- project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/controller/UserController.java
+++ project-03-cookie-based-jwt-auth/cookie_based_jwt_auth/src/main/java/lk/spring_security/cookie_based_jwt_auth/web/user/controller/UserController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.cookie_based_jwt_auth.web.user.controller;
 
+import lk.spring_security.cookie_based_jwt_auth.usecase.user.UserUseCase;
+import lk.spring_security.cookie_based_jwt_auth.web.user.webMapper.UserWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    //inject required class
+    private final UserUseCase userUseCase;
+    private final UserWebMapper userWebMapper;
+
+    public UserController(
+            UserUseCase userUseCase,
+            UserWebMapper userWebMapper
+    ) {
+        this.userUseCase = userUseCase;
+        this.userWebMapper = userWebMapper;
+    }
 }
