47141fbed6dc8c06112a74bef12f95452105a104
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-18 20:56:08 +0530

    feat: initiate product domain model and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/models/Product.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/models/Product.java
@@ -1,4 +1,14 @@
 package lk.spring_security.refresh_token.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    private Long productId;
+    private String productName;
+    private Double productPrice;
 }
