ff97158cf2908f18a5552527fbe5dac0467f5560
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:08:17 +0530

    feat: initiate user entity and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/entities/UserEntity.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/entities/UserEntity.java
@@ -1,4 +1,29 @@
 package lk.spring_security.refresh_token.infrastructure.user.entities;
 
+import jakarta.persistence.*;
+import lk.spring_security.refresh_token.domain.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
