9dab7902c69da8c3a4fcf2e28b6749e3c2117e4a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 19:27:02 +0530

    feat: create custom query for get user by email

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/jpa/JpaUserRepository.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/jpa/JpaUserRepository.java
@@ -3,5 +3,10 @@ package lk.spring_security.refresh_token.infrastructure.user.jpa;
 import lk.spring_security.refresh_token.infrastructure.user.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+
+    //user find by email
+    Optional<UserEntity> findByEmail(String email);
 }
