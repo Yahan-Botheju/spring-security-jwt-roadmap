465b6efa9a6c4b725be38d049fef3a37de64ea6b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:15:51 +0530

    feat: initiate refresh token entity and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/entities/RefreshTokenEntity.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/entities/RefreshTokenEntity.java
@@ -1,4 +1,29 @@
 package lk.spring_security.refresh_token.infrastructure.user.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_token")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "userId", nullable = false)
+    private UserEntity user;
 }
