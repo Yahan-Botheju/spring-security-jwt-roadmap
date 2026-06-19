dd7af5d6b978b81bc8fee84a5b35ab17a58ff265
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 19:39:39 +0530

    feat: setup  user get by email helper method in persistence layer

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/UserRepositoryImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/UserRepositoryImpl.java
@@ -1,4 +1,30 @@
 package lk.spring_security.refresh_token.infrastructure.user;
 
-public class UserRepositoryImpl {
+import lk.spring_security.refresh_token.domain.models.User;
+import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+import lk.spring_security.refresh_token.infrastructure.user.jpa.JpaUserRepository;
+import lk.spring_security.refresh_token.infrastructure.user.mappers.UserPersistenceMapper;
+
+import java.util.Optional;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    //inject required dependencies
+    private final JpaUserRepository jpaUserRepository;
+    private final UserPersistenceMapper userPersistenceMapper;
+
+
+    public UserRepositoryImpl(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ) {
+        this.jpaUserRepository = jpaUserRepository;
+        this.userPersistenceMapper = userPersistenceMapper;
+    }
+
+    //user find by email
+    @Override
+    public Optional<User> findByEmail(String email){
+        return jpaUserRepository.findByEmail(email).map(userPersistenceMapper::toDomainMode);
+    }
 }
