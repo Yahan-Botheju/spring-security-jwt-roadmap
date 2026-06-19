2dd75d9c13e4ed2e0e091712cae46166cbf6143f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:04:11 +0530

    feat: initiate product entity and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/entities/ProductEntity.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/entities/ProductEntity.java
@@ -1,4 +1,24 @@
 package lk.spring_security.refresh_token.infrastructure.product.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Double productPrice;
 }
