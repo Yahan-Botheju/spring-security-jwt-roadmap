e1b4c32a3c3e2fc397424f55e9a9e95e79598639
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 19:37:50 +0530

    feat: initiate user persistence mapper

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/mappers/UserPersistenceMapper.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/mappers/UserPersistenceMapper.java
@@ -1,4 +1,14 @@
 package lk.spring_security.refresh_token.infrastructure.user.mappers;
 
+import lk.spring_security.refresh_token.domain.models.User;
+import lk.spring_security.refresh_token.infrastructure.user.entities.UserEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserPersistenceMapper {
+    //domain model to entity
+    UserEntity toEntity(User user);
+
+    //entity to domain model
+    User toDomainMode(UserEntity userEntity);
 }
