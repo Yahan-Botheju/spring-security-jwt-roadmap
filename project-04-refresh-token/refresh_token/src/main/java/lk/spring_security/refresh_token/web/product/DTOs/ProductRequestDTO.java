c34c32883614f1f63a56e86d209790e1c04dcd68
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:43:07 +0530

    feat: initiate product request dto and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/DTOs/ProductRequestDTO.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/DTOs/ProductRequestDTO.java
@@ -1,4 +1,20 @@
 package lk.spring_security.refresh_token.web.product.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequestDTO {
+    @NotBlank(message = "Product name cannot be empty")
+    private String productName;
+
+    @NotNull(message = "Price cannot be empty")
+    @Positive(message = "Price must be positive value")
+    private double productPrice;
 }
