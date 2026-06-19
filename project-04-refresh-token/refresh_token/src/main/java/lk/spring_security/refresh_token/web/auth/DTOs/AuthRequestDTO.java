524d1e569199060b2e5b1ccc1ea993b987b1a9b7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:38:45 +0530

    feat: initiate auth request dto and its fields

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/AuthRequestDTO.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/AuthRequestDTO.java
@@ -1,4 +1,21 @@
 package lk.spring_security.refresh_token.web.auth.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at leat 6 character long")
+    private String password;
 }
