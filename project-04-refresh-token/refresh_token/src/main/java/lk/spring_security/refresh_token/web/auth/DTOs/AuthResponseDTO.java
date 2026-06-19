9bd7d611dc19e093c6fe45d142c432d9f128cb95
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-19 11:39:14 +0530

    feat: initiate auth response dto

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/AuthResponseDTO.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/AuthResponseDTO.java
@@ -1,4 +1,14 @@
 package lk.spring_security.refresh_token.web.auth.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponseDTO {
+    private String status;
+    private String message;
+
 }
