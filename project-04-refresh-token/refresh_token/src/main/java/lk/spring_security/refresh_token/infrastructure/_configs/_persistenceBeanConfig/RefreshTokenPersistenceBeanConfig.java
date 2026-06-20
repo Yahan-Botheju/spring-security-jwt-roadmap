37cfa3ee938ca17d868860001261ca337302a965
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 12:34:57 +0530

    feat: initiate refresh token repo impl as bean through configuration classes

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_persistenceBeanConfig/RefreshTokenPersistenceBeanConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_persistenceBeanConfig/RefreshTokenPersistenceBeanConfig.java
@@ -1,6 +1,18 @@
 package lk.spring_security.refresh_token.infrastructure._configs._persistenceBeanConfig;
 
+import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.infrastructure.refreshToken.RefreshTokenRepositoryImpl;
+import lk.spring_security.refresh_token.infrastructure.refreshToken.jpa.JpaRefreshTokenRepository;
+import lk.spring_security.refresh_token.infrastructure.refreshToken.mapper.RefreshTokenPersistenceMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@Configuration
 public class RefreshTokenPersistenceBeanConfig {
+    @Bean
+    public RefreshTokenRepository refreshTokenRepository(
+            JpaRefreshTokenRepository jpaRefreshTokenRepository,
+            RefreshTokenPersistenceMapper refreshTokenPersistenceMapper
+    ) {
+        return new RefreshTokenRepositoryImpl(jpaRefreshTokenRepository, refreshTokenPersistenceMapper);
+    }
 }
