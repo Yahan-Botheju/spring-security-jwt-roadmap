0a915164e97d309f3d7abe7e696bc03a204224a4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 12:54:28 +0530

    feat: inject user repo

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/refreshToken/RefreshTokenRepositoryImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/refreshToken/RefreshTokenRepositoryImpl.java
@@ -2,9 +2,11 @@ package lk.spring_security.refresh_token.infrastructure.refreshToken;
 
 import lk.spring_security.refresh_token.domain.models.RefreshToken;
 import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.domain.repositories.UserRepository;
 import lk.spring_security.refresh_token.infrastructure.refreshToken.entities.RefreshTokenEntity;
 import lk.spring_security.refresh_token.infrastructure.refreshToken.jpa.JpaRefreshTokenRepository;
 import lk.spring_security.refresh_token.infrastructure.refreshToken.mapper.RefreshTokenPersistenceMapper;
+import lk.spring_security.refresh_token.infrastructure.user.entities.UserEntity;
 
 import java.util.Optional;
 
@@ -13,14 +15,16 @@ public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
     //inject required dependencies
     private final JpaRefreshTokenRepository jpaRefreshTokenRepository;
     private final RefreshTokenPersistenceMapper refreshTokenPersistenceMapper;
-
+    private final UserRepository userRepository;
 
     public RefreshTokenRepositoryImpl(
             JpaRefreshTokenRepository jpaRefreshTokenRepository,
-            RefreshTokenPersistenceMapper refreshTokenPersistenceMapper
+            RefreshTokenPersistenceMapper refreshTokenPersistenceMapper,
+            UserRepository userRepository
     ) {
         this.jpaRefreshTokenRepository = jpaRefreshTokenRepository;
         this.refreshTokenPersistenceMapper = refreshTokenPersistenceMapper;
+        this.userRepository = userRepository;
     }
 
     //check token availability
@@ -40,5 +44,12 @@ public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
         RefreshTokenEntity savedToken = jpaRefreshTokenRepository.save(tokenEntity);
 
         return refreshTokenPersistenceMapper.toDomainModel(savedToken);
+    }
+
+    //remove token from db when user log out
+    @Override
+    public void deleteByUserEmail(String email){
+        UserEntity checkUserAvailability = userRepository.findByEmail(email)
+                .orElseThrow()
     }
 }
