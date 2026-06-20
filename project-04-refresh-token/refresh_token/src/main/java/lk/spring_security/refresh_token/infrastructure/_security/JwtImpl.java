783450cad192e6e8f804d20c14db44bfeacdd84e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 13:23:03 +0530

    feat: generate avaialble classes in domain repo

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/JwtImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/JwtImpl.java
@@ -1,6 +1,7 @@
 package lk.spring_security.refresh_token.infrastructure._security;
 
 import lk.spring_security.refresh_token.domain.repositories.TokenService;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
 
@@ -12,5 +13,20 @@ public class JwtImpl implements TokenService {
 
     public JwtImpl(SecretKey secretKey) {
         this.secretKey = secretKey;
+    }
+
+    @Override
+    public String extractToken(String token) {
+        return "";
+    }
+
+    @Override
+    public String generateAccessToken(UserDetails userDetails, Long userId) {
+        return "";
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return false;
     }
 }
