71274df9541fa7f629843e9281083c970f00ad5e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 12:43:33 +0530

    feat: initiate token availability check method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/repositories/RefreshTokenRepository.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/repositories/RefreshTokenRepository.java
@@ -1,4 +1,11 @@
 package lk.spring_security.refresh_token.domain.repositories;
 
+import lk.spring_security.refresh_token.domain.models.RefreshToken;
+
+import java.util.Optional;
+
 public interface RefreshTokenRepository {
+
+    //check token availability of db
+    Optional<RefreshToken> findByToken(String token);
 }
