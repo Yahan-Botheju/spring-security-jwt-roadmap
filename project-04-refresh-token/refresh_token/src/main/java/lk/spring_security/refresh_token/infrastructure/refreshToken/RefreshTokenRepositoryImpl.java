8778e782af70b3b4f34961564f6d9a72848c9b25
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 12:35:25 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/refreshToken/RefreshTokenRepositoryImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/refreshToken/RefreshTokenRepositoryImpl.java
@@ -1,4 +1,21 @@
 package lk.spring_security.refresh_token.infrastructure.refreshToken;
 
-public class RefreshTokenRepositoryImpl {
+import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.infrastructure.refreshToken.jpa.JpaRefreshTokenRepository;
+import lk.spring_security.refresh_token.infrastructure.refreshToken.mapper.RefreshTokenPersistenceMapper;
+
+public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
+
+    //inject required dependencies
+    private final JpaRefreshTokenRepository jpaRefreshTokenRepository;
+    private final RefreshTokenPersistenceMapper refreshTokenPersistenceMapper;
+
+
+    public RefreshTokenRepositoryImpl(
+            JpaRefreshTokenRepository jpaRefreshTokenRepository,
+            RefreshTokenPersistenceMapper refreshTokenPersistenceMapper
+    ) {
+        this.jpaRefreshTokenRepository = jpaRefreshTokenRepository;
+        this.refreshTokenPersistenceMapper = refreshTokenPersistenceMapper;
+    }
 }
