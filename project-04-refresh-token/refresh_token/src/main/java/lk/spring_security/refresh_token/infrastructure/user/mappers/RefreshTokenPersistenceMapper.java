700ccb2912b06594987f4d3b3cbc4c3fbd50500f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-20 11:53:38 +0530

    feat: initiate refresh token persistence mapper

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/mappers/RefreshTokenPersistenceMapper.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/user/mappers/RefreshTokenPersistenceMapper.java
@@ -1,4 +1,16 @@
 package lk.spring_security.refresh_token.infrastructure.user.mappers;
 
+import lk.spring_security.refresh_token.domain.models.RefreshToken;
+import lk.spring_security.refresh_token.infrastructure.user.entities.RefreshTokenEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {UserPersistenceMapper.class})
 public interface RefreshTokenPersistenceMapper {
+
+    //domain model to entity
+    RefreshTokenEntity toEntity(RefreshToken refreshToken);
+
+    //entity to domain model
+    RefreshToken toDomainModel(RefreshTokenEntity refreshTokenEntity);
+
 }
