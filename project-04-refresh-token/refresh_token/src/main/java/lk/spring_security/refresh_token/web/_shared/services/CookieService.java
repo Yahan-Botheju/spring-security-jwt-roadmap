c3f3106ea2b26516ac9d94b1da0d18baec0ab8e2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-21 13:11:41 +0530

    feat: initiate clear token method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/_shared/services/CookieService.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/_shared/services/CookieService.java
@@ -5,4 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public interface CookieService {
     //access token set as Http only cookie
     void setAccessTokenCookie(HttpServletResponse response, String accessToken);
+
+    //refresh toke as http only cookie
+    void setRefreshTokenCookie(HttpServletResponse response, String refreshToken);
+
+    //clear tokens
+    void clearCookies(HttpServletResponse response);
+
 }
