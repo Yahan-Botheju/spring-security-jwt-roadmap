164b0538ff0c0e22a3d0d7b5553e76516111d6ac
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-21 13:05:23 +0530

    feat: initiate access token cookie method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/_shared/services/CookieService.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/_shared/services/CookieService.java
@@ -1,4 +1,8 @@
 package lk.spring_security.refresh_token.web._shared.services;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface CookieService {
+    //access token set as Http only cookie
+    void setAccessTokenCookie(HttpServletResponse response, String accessToken);
 }
