b13d902a8dc917a3a66e6422dd837d9de966fec5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-22 21:33:43 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/filter/JwtAuthenticationFilter.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,26 @@
 package lk.spring_security.refresh_token.infrastructure._security.filter;
 
+import lk.spring_security.refresh_token.domain.repositories.TokenService;
+import lk.spring_security.refresh_token.web._shared.services.CookieService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //inject required dependencies
+    private final TokenService tokenService;
+    private final CookieService cookieService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(
+            TokenService tokenService,
+            CookieService cookieService,
+            UserDetailsService userDetailsService
+    ) {
+        this.tokenService = tokenService;
+        this.cookieService = cookieService;
+        this.userDetailsService = userDetailsService;
+    }
 }
