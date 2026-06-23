9593bc77f60d985e793d0350d9aebf4bcb5888e3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-23 21:05:10 +0530

    feat: initiate product web mapper

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/webMappers/ProductWebMapper.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/webMappers/ProductWebMapper.java
@@ -1,4 +1,15 @@
 package lk.spring_security.refresh_token.web.product.webMappers;
 
+import lk.spring_security.refresh_token.domain.models.Product;
+import lk.spring_security.refresh_token.web.product.DTOs.ProductRequestDTO;
+import lk.spring_security.refresh_token.web.product.DTOs.ProductResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ProductWebMapper {
+    //request to domain model
+    Product toDomainModel(ProductRequestDTO productRequestDTO);
+
+    //domain model to responseDTO
+    ProductResponseDTO toResponseDTO(Product product);
 }
