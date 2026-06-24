342fb6dbefa1d2816e911da655fe638a07f26329
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-24 23:11:57 +0530

    feat: generate loadUserByUsername method and get user from db

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package lk.spring_security.refresh_token.infrastructure._security.user_spring_wrapper;
 
 import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -10,5 +13,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    //find user from db
+    @Override
+    public UserDetails loadUserByUsername(
+            @NonNull String username
+    ) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
