55281d8a30d1bebeaae0f9726393c2b94e27a881
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-24 23:01:02 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
@@ -1,7 +1,14 @@
 package lk.spring_security.refresh_token.infrastructure._security.user_spring_wrapper;
 
+import lk.spring_security.refresh_token.domain.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
+    //inject required dependencies
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
