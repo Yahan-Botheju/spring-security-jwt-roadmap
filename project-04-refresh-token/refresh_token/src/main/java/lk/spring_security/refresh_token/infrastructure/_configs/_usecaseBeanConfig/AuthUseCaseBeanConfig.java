611d2809309ba3b348453aec0fb1217247046307
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-25 22:13:18 +0530

    feat: create configuration class to initiate auth usecase impl as bean

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/AuthUseCaseBeanConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/AuthUseCaseBeanConfig.java
@@ -1,4 +1,23 @@
 package lk.spring_security.refresh_token.infrastructure._configs._usecaseBeanConfig;
 
+import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+import lk.spring_security.refresh_token.usecase.auth.AuthUseCase;
+import lk.spring_security.refresh_token.usecase.auth.AuthUseCaseImpl;
+import lk.spring_security.refresh_token.web._shared.services.CookieService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class AuthUseCaseBeanConfig {
+    @Bean
+    public AuthUseCase authUseCase(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            CookieService cookieService,
+            AuthenticationManager authenticationManager
+    ) {
+        return new AuthUseCaseImpl(userRepository,passwordEncoder, cookieService, authenticationManager);
+    }
 }
