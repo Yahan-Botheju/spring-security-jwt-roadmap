c8e72a72c11abcbaacc52db41f4f1a4361ab1dfd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-25 22:20:49 +0530

    feat: create configuration class for initiate product use case as bean

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/ProductUseCaseBeanConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/ProductUseCaseBeanConfig.java
@@ -1,4 +1,17 @@
 package lk.spring_security.refresh_token.infrastructure._configs._usecaseBeanConfig;
 
+import lk.spring_security.refresh_token.domain.repositories.ProductRepository;
+import lk.spring_security.refresh_token.usecase.product.ProductUseCase;
+import lk.spring_security.refresh_token.usecase.product.ProductUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ProductUseCaseBeanConfig {
+    @Bean
+    public ProductUseCase  productUseCase(
+            ProductRepository productRepository
+    ) {
+        return new ProductUseCaseImpl(productRepository);
+    }
 }
