15d0583fddb935b9dd3e1fe8aee9c1a4109e0362
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-25 22:06:41 +0530

    feat: create config class to initate product persistence as bean

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_persistenceBeanConfig/ProductPersistenceBeanConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_persistenceBeanConfig/ProductPersistenceBeanConfig.java
@@ -1,4 +1,19 @@
 package lk.spring_security.refresh_token.infrastructure._configs._persistenceBeanConfig;
 
+import lk.spring_security.refresh_token.domain.repositories.ProductRepository;
+import lk.spring_security.refresh_token.infrastructure.product.ProductRepositoryImpl;
+import lk.spring_security.refresh_token.infrastructure.product.jpa.JpaProductRepository;
+import lk.spring_security.refresh_token.infrastructure.product.mappers.ProductPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ProductPersistenceBeanConfig {
+    @Bean
+    public ProductRepository productRepository(
+            JpaProductRepository jpaProductRepository,
+            ProductPersistenceMapper productPersistenceMapper
+    ) {
+        return new ProductRepositoryImpl(jpaProductRepository, productPersistenceMapper);
+    }
 }
