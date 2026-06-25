d4bc544d20c7e25f3844d3d244ddeb31ac56d09b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-25 22:08:17 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/ProductRepositoryImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/ProductRepositoryImpl.java
@@ -1,4 +1,21 @@
 package lk.spring_security.refresh_token.infrastructure.product;
 
-public class ProductRepositoryImpl {
+import lk.spring_security.refresh_token.domain.repositories.ProductRepository;
+import lk.spring_security.refresh_token.infrastructure.product.jpa.JpaProductRepository;
+import lk.spring_security.refresh_token.infrastructure.product.mappers.ProductPersistenceMapper;
+
+public class ProductRepositoryImpl implements ProductRepository {
+
+    //inject required dependencies
+    private final JpaProductRepository jpaProductRepository;
+    private final ProductPersistenceMapper productPersistenceMapper;
+
+
+    public ProductRepositoryImpl(
+            JpaProductRepository jpaProductRepository,
+            ProductPersistenceMapper productPersistenceMapper
+    ) {
+        this.jpaProductRepository = jpaProductRepository;
+        this.productPersistenceMapper = productPersistenceMapper;
+    }
 }
