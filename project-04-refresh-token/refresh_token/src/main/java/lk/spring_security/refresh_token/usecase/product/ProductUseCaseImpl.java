d3f51a8549aa3249dd78269038d72362abbb9173
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-25 22:21:18 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/product/ProductUseCaseImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/product/ProductUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.spring_security.refresh_token.usecase.product;
 
-public class ProductUseCaseImpl {
+import lk.spring_security.refresh_token.domain.repositories.ProductRepository;
+
+public class ProductUseCaseImpl implements  ProductUseCase {
+
+    //inject required dependencies
+    private final  ProductRepository productRepository;
+
+    public ProductUseCaseImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
