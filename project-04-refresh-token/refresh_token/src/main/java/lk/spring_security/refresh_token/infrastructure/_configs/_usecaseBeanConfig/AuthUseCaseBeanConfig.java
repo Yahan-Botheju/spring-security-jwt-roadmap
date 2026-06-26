1110132afa1dc2eb500ba0bcfa941ef72572d917
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-26 21:23:05 +0530

    feat: initiate another two dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/AuthUseCaseBeanConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_configs/_usecaseBeanConfig/AuthUseCaseBeanConfig.java
@@ -1,5 +1,7 @@
 package lk.spring_security.refresh_token.infrastructure._configs._usecaseBeanConfig;
 
+import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.domain.repositories.TokenService;
 import lk.spring_security.refresh_token.domain.repositories.UserRepository;
 import lk.spring_security.refresh_token.usecase.auth.AuthUseCase;
 import lk.spring_security.refresh_token.usecase.auth.AuthUseCaseImpl;
@@ -13,11 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AuthUseCaseBeanConfig {
     @Bean
     public AuthUseCase authUseCase(
+            RefreshTokenRepository refreshTokenRepository,
             UserRepository userRepository,
             PasswordEncoder passwordEncoder,
             CookieService cookieService,
+            TokenService tokenService,
             AuthenticationManager authenticationManager
     ) {
-        return new AuthUseCaseImpl(userRepository,passwordEncoder, cookieService, authenticationManager);
+        return new AuthUseCaseImpl(refreshTokenRepository ,userRepository,passwordEncoder, cookieService,tokenService ,authenticationManager);
     }
 }
