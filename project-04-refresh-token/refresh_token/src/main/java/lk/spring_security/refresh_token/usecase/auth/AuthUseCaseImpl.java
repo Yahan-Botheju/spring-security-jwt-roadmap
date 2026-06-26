d27e920f403fdff8086cb244ce8a39684e48868f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-26 21:23:35 +0530

    feat: inject required dependancies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/AuthUseCaseImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/AuthUseCaseImpl.java
@@ -1,5 +1,7 @@
 package lk.spring_security.refresh_token.usecase.auth;
 
+import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.domain.repositories.TokenService;
 import lk.spring_security.refresh_token.domain.repositories.UserRepository;
 import lk.spring_security.refresh_token.web._shared.services.CookieService;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,21 +10,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AuthUseCaseImpl implements AuthUseCase{
 
     //inject required dependencies
+    private final RefreshTokenRepository refreshTokenRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final CookieService cookieService;
+    private final TokenService tokenService;
     private final AuthenticationManager authenticationManager;
 
 
     public AuthUseCaseImpl(
+            RefreshTokenRepository refreshTokenRepository,
             UserRepository userRepository,
             PasswordEncoder passwordEncoder,
             CookieService cookieService,
+            TokenService tokenService,
             AuthenticationManager authenticationManager
     ) {
+        this.refreshTokenRepository = refreshTokenRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.cookieService = cookieService;
+        this.tokenService = tokenService;
         this.authenticationManager = authenticationManager;
     }
+
+
 }
