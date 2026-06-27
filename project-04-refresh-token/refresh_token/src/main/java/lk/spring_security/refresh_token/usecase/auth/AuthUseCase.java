2a98291215c6f11ae5f428294d7db5a021a5f0d3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-27 20:08:48 +0530

    feat: initiate register user method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/AuthUseCase.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/AuthUseCase.java
@@ -1,4 +1,9 @@
 package lk.spring_security.refresh_token.usecase.auth;
 
+import lk.spring_security.refresh_token.domain.models.User;
+
 public interface AuthUseCase {
+
+    //register new user
+    User registerUser(User user);
 }
