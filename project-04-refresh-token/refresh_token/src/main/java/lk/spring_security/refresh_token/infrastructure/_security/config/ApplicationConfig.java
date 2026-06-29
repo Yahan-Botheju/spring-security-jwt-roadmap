cba85064fb3920bce080758ca031a4b92738d1e9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-29 17:43:54 +0530

    feat: create password encoder

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/config/ApplicationConfig.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/config/ApplicationConfig.java
@@ -1,4 +1,16 @@
 package lk.spring_security.refresh_token.infrastructure._security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ApplicationConfig {
+
+    //password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
