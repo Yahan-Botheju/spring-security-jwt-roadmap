2d0537db0ed1dcfe74cfd08a652e8df2dd3caf96
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-29 17:52:38 +0530

    feat: initiate token extractor

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/token_extraction/TokenExtractor.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/token_extraction/TokenExtractor.java
@@ -1,4 +1,10 @@
 package lk.spring_security.refresh_token.infrastructure._security.token_extraction;
 
-public class TokenExtractor {
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+public interface TokenExtractor {
+    //initiate token extractor
+    Optional<String> extractTokenFromCookie(HttpServletRequest request);
 }
