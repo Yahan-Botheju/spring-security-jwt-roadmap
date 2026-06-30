1ae60766b43f05186d516c155ac40026961d0f9c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-06-30 21:00:49 +0530

    feat: create new impl class and initiate token find by values

---
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/token_extraction/TokenExtractorImpl.java
@@ -0,0 +1,20 @@
+package lk.spring_security.refresh_token.infrastructure._security.token_extraction;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+public class TokenExtractorImpl implements  TokenExtractor {
+
+    //create cookie array which finds value by token name
+    private Optional<String> extractCookieByName(HttpServletRequest request, String cookieName) {
+        if(request.getCookies() != null){
+            for(Cookie cookie : request.getCookies()){
+                if(cookie.getName().equals(cookieName)){
+                    return Optional.of(cookie.getValue());
+                }
+            }
+        }
+    }
+}
