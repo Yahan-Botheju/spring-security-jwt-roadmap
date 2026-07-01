da902d2ccc90346886ded8f14aaf5c66e6d3d95e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-01 21:35:51 +0530

    feat: inject required dependencies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.refresh_token.web.auth.controllers;
 
+import lk.spring_security.refresh_token.usecase.auth.AuthUseCase;
+import lk.spring_security.refresh_token.web.auth.webMappers.AuthWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    //inject required dependencies
+    private final AuthUseCase authUseCase;
+    private final AuthWebMapper authWebMapper;
+
+    public AuthController(
+            AuthUseCase authUseCase,
+            AuthWebMapper authWebMapper
+    ) {
+        this.authUseCase = authUseCase;
+        this.authWebMapper = authWebMapper;
+    }
 }
