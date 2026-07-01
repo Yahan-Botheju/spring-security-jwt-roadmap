2a3c146eae87f5001119fd7de89cd32b8a735241
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-01 21:17:41 +0530

    fix: use cookie service to get access token

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/filter/JwtAuthenticationFilter.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/_security/filter/JwtAuthenticationFilter.java
@@ -6,13 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.spring_security.refresh_token.domain.repositories.CookieService;
 import lk.spring_security.refresh_token.domain.repositories.TokenService;
-import lk.spring_security.refresh_token.infrastructure._security.token_extraction.TokenExtractor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -45,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
 
         //get access token from cookie
-        final String accessToken = tokenExtractor.extractAccessTokenFromCookie(request).orElse(null) ;
+        final String accessToken = cookieService.extractCookieByName(request, "accessToken");
         final String userEmail;
 
         //token is null do filter
