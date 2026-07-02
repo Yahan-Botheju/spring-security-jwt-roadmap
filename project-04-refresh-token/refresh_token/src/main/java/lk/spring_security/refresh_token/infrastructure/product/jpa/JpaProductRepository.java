c75a6c2cc192475e58dc9a713015ae21073d5187
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-02 21:51:56 +0530

    feat: extenda with jpa repo

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/jpa/JpaProductRepository.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/jpa/JpaProductRepository.java
@@ -1,4 +1,8 @@
 package lk.spring_security.refresh_token.infrastructure.product.jpa;
 
-public interface JpaProductRepository {
+import lk.spring_security.refresh_token.infrastructure.product.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaProductRepository extends JpaRepository<ProductEntity, Long>
+{
 }
