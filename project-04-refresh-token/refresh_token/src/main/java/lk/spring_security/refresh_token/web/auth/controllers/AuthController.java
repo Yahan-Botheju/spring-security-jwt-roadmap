a9e46050d3f2224a1a289a53f861deef03d00a13
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-02 11:26:09 +0530

    feat: set values to auh users

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package lk.spring_security.refresh_token.web.auth.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lk.spring_security.refresh_token.domain.models.User;
 import lk.spring_security.refresh_token.usecase.auth.AuthUseCase;
@@ -38,5 +40,16 @@ public class AuthController {
         authUseCase.registerUser(toDomainModel);
 
         return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+
+    //login route
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(
+            @Valid @RequestBody AuthRequestDTO authRequestDTO,
+            HttpServletResponse servletResponse
+    ){
+        //set email, paw and token for auth user
+        authUseCase.loginUser(authRequestDTO.getEmail(), authRequestDTO.getPassword(), servletResponse);
+
     }
 }
