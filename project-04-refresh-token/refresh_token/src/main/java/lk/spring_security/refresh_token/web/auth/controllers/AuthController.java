7dc7ca7dcd7a9422faa7ce5a8076fcd82cede3de
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-02 11:19:14 +0530

    feat: create register route

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/controllers/AuthController.java
@@ -1,7 +1,15 @@
 package lk.spring_security.refresh_token.web.auth.controllers;
 
+import jakarta.validation.Valid;
+import lk.spring_security.refresh_token.domain.models.User;
 import lk.spring_security.refresh_token.usecase.auth.AuthUseCase;
+import lk.spring_security.refresh_token.web.auth.DTOs.AuthRequestDTO;
+import lk.spring_security.refresh_token.web.auth.DTOs.AuthResponseDTO;
 import lk.spring_security.refresh_token.web.auth.webMappers.AuthWebMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +27,16 @@ public class AuthController {
     ) {
         this.authUseCase = authUseCase;
         this.authWebMapper = authWebMapper;
+    }
+
+    //register user
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(
+            @Valid @RequestBody AuthRequestDTO authRequestDTO
+            ){
+        User toDomainModel = authWebMapper.toDomainModel(authRequestDTO);
+        authUseCase.registerUser(toDomainModel);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
 }
