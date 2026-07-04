07d386153ef1121d183692f5ed73c7cc78b58953
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-04 22:15:32 +0530

    feat: initiate product found by id method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/repositories/ProductRepository.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/domain/repositories/ProductRepository.java
@@ -2,7 +2,12 @@ package lk.spring_security.refresh_token.domain.repositories;
 
 import lk.spring_security.refresh_token.domain.models.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository {
+
+    //product find by id
+    Optional<Product> productFindById(Long productId);
 
     //save products
     Product saveProducts(Product product);
