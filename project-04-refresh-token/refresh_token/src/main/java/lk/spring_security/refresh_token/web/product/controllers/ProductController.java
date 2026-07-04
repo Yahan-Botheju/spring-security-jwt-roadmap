9339fd51aaeca9ee44f6d84bdb3db646cca42230
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-04 22:21:51 +0530

    feat: inject required dependencies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/controllers/ProductController.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/product/controllers/ProductController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.refresh_token.web.product.controllers;
 
+import lk.spring_security.refresh_token.usecase.product.ProductUseCase;
+import lk.spring_security.refresh_token.web.product.webMappers.ProductWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    //inject required dependencies
+    private final ProductUseCase productUseCase;
+    private final ProductWebMapper productWebMapper;
+
+    public ProductController(
+            ProductUseCase productUseCase,
+            ProductWebMapper productWebMapper
+    ) {
+        this.productUseCase = productUseCase;
+        this.productWebMapper = productWebMapper;
+    }
 }
