0607aa694733d0e8e4e383409a41c9393ca37aa7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-04 22:16:08 +0530

    feat: create custom query for get product by id

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/jpa/JpaProductRepository.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/jpa/JpaProductRepository.java
@@ -1,8 +1,15 @@
 package lk.spring_security.refresh_token.infrastructure.product.jpa;
 
+import lk.spring_security.refresh_token.domain.models.Product;
 import lk.spring_security.refresh_token.infrastructure.product.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaProductRepository extends JpaRepository<ProductEntity, Long>
-{
+import java.util.Optional;
+
+public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    //product find by id
+    Optional<Product> productFindById(Long productId);
+
+
 }
