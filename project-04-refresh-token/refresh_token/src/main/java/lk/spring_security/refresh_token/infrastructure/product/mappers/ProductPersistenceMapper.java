c59887364aafe477bbb0fef46e12badb02584583
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-05 19:41:02 +0530

    feat: initiate update product mapper

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/mappers/ProductPersistenceMapper.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/infrastructure/product/mappers/ProductPersistenceMapper.java
@@ -3,6 +3,7 @@ package lk.spring_security.refresh_token.infrastructure.product.mappers;
 import lk.spring_security.refresh_token.domain.models.Product;
 import lk.spring_security.refresh_token.infrastructure.product.entities.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductPersistenceMapper {
@@ -11,4 +12,7 @@ public interface ProductPersistenceMapper {
 
     //entity to domain model
     Product toDomainModel(ProductEntity  productEntity);
+
+    //update product
+    ProductEntity updateEntity(Product product, @MappingTarget ProductEntity productEntity);
 }
