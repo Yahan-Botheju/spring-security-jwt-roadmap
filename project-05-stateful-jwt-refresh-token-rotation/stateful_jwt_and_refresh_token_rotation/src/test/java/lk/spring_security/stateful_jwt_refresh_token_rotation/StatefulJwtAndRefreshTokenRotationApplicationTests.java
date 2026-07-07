2c21da1e554194e22041dc057e91b1989366c7b3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-07 19:43:47 +0530

    initiate project-05

---
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/test/java/lk/spring_security/stateful_jwt_refresh_token_rotation/StatefulJwtAndRefreshTokenRotationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.spring_security.stateful_jwt_refresh_token_rotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefulJwtAndRefreshTokenRotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
