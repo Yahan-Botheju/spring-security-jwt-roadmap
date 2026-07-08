331b606b38e173d21bcea13d9e8c3e7b86a12b2a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-08 23:45:34 +0530

    feat: initiate user domain model fields

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/User.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/User.java
@@ -1,4 +1,18 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    private Long userId;
+    private String email;
+    private String password;
+
+    private Role role;
 }
