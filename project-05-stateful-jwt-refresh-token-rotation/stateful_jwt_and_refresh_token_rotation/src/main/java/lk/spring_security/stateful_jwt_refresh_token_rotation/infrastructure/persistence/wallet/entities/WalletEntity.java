ff256163acfc976ab5b73e127db164881cdf4a4f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-09 21:35:00 +0530

    feat: initiate wallet entity

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/wallet/entities/WalletEntity.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/wallet/entities/WalletEntity.java
@@ -1,4 +1,23 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.wallet.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wallets")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class WalletEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long walletId;
+
+    @Column(nullable = false)
+    private Double walletBalance;
+
 }
