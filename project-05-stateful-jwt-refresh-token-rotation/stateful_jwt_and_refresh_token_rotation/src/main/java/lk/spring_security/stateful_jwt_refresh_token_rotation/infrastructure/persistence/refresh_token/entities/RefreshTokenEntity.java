4e9beab9eae0f09ed8935eac04de05c84d8f4072
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-09 21:58:29 +0530

    feat: initiate refresh token entity

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/refresh_token/entities/RefreshTokenEntity.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/refresh_token/entities/RefreshTokenEntity.java
@@ -1,4 +1,33 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.refresh_token.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RefreshTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tokenId;
+
+    @Column(unique = true,nullable = false)
+    private String token;
+
+    @Column(nullable = false, name = "expiry_date")
+    private Instant expiryDate;
+
+    @Column(nullable = false, name = "is_used")
+    private boolean isUsed;
+
+    @Column(nullable = false, name = "is_revoked")
+    private boolean isRevoked;
 }
