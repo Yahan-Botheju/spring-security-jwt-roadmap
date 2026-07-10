44f533271d98dbbdfbcab1e9bf62bae990b7f49d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-10 21:02:10 +0530

    feat: initiate wallet persistence impl as bean through the configuration class

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_persistenceBeanConfig/WalletPersistenceBeanConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_persistenceBeanConfig/WalletPersistenceBeanConfig.java
@@ -1,4 +1,19 @@
-package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._usecaseBeanConfig;
+package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._persistenceBeanConfig;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.wallet.WalletRepositoryImpl;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.wallet.jpa.JpaWalletRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.wallet.mappers.WalletPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WalletPersistenceBeanConfig {
+    @Bean
+    public WalletRepository walletRepository(
+            JpaWalletRepository jpaWalletRepository,
+            WalletPersistenceMapper walletPersistenceMapper
+    ){
+        return new WalletRepositoryImpl(jpaWalletRepository,walletPersistenceMapper);
+    }
 }
