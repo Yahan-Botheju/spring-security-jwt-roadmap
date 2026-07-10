e406a8a6bfe694960cfcad58e189ffd7b2f1dfb8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-10 20:50:42 +0530

    feat: initiate refresh token persistence impl as bean through configuration class

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_persistenceBeanConfig/RefreshTokenPersistenceBeanConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_persistenceBeanConfig/RefreshTokenPersistenceBeanConfig.java
@@ -1,4 +1,25 @@
-package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._usecaseBeanConfig;
+package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._persistenceBeanConfig;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.RefreshTokenReposiroty;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.refresh_token.RefreshTokenRepositoryImpl;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.refresh_token.jpa.JpaRefreshTokenRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.refresh_token.mappers.RefreshTokenPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RefreshTokenPersistenceBeanConfig {
+    @Bean
+    public RefreshTokenReposiroty refreshTokenReposiroty(
+            JpaRefreshTokenRepository jpaRefreshTokenRepository,
+            RefreshTokenPersistenceMapper refreshTokenPersistenceMapper,
+            UserRepository userRepository
+    ){
+        return new RefreshTokenRepositoryImpl(
+                jpaRefreshTokenRepository,
+                refreshTokenPersistenceMapper,
+                userRepository
+        );
+    }
 }
