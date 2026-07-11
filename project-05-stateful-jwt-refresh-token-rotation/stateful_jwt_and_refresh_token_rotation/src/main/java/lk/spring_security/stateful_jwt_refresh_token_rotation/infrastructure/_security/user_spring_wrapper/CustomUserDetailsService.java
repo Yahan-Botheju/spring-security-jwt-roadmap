e308a3d537120c35af9e29a0ec238bc46eceda0d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-11 21:28:23 +0530

    feat: inject user repo

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/user_spring_wrapper/CustomUserDetailsService.java
@@ -1,4 +1,14 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.user_spring_wrapper;
 
-public class CustomUserDetailsService {
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    //inject user repo
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
