835ed979f59f72e360a99385808ffd4b22fbcc6d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-11 21:22:20 +0530

    feat: initiate user role for spring secutiy

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/user_spring_wrapper/CustomUserDetails.java
@@ -3,6 +3,7 @@ package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._s
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -17,9 +18,14 @@ public class CustomUserDetails implements UserDetails {
         this.user=user;
     }
 
+    public User getUser() {
+        return this.user;
+    }
+
+    //initiate role to spring security
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.user.getRole().name()));
     }
 
     @Override
