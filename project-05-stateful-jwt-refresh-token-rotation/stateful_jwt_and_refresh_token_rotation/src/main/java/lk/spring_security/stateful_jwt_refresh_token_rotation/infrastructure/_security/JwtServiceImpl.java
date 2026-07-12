5e0e9b39733147c3a7746e1e26b817553ba206c5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-12 22:18:35 +0530

    feat: create access token generate method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/JwtServiceImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/JwtServiceImpl.java
@@ -1,11 +1,15 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security;
 
 import io.jsonwebtoken.Jwts;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.user_spring_wrapper.CustomUserDetails;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.user_spring_wrapper.CustomUserDetailsService;
 import lombok.Data;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JwtServiceImpl implements TokenService {
@@ -25,6 +29,23 @@ public class JwtServiceImpl implements TokenService {
         this.accessTokenExpirationMs = accessTokenExpirationMs;
         this.refreshTokenExpirationMs = refreshTokenExpirationMs;
     }
+
+    /* __GENERATE TOKENS__ */
+
+    //generate access token
+    @Override
+    public String generateAccessToken(User user){
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", user.getRole().name()); //include role
+        extraClaims.put("userId",  user.getUserId()); //include user ID
+
+        //wrap using spring custom user details
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        return buildToken(extraClaims, customUserDetails.getUsername(), accessTokenExpirationMs);
+    }
+
+
 
      /* __JWT HELPER METHODS__ */
 
