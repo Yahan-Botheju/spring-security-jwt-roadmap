20305d5c536876f59bce8c5f79d42d5b891fd9fa
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-12 23:00:57 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/_shared/service/CookieServiceImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/_shared/service/CookieServiceImpl.java
@@ -1,4 +1,21 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web._shared.service;
 
-public class CookieServiceImpl {
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.CookieService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CookieServiceImpl implements CookieService {
+
+    //inject required dependencies
+    @Value("${application.security.cookie-name}")
+    private final String COOKIE_NAME;
+
+    @Value("${application.security.cookie.refresh-token-expiry-seconds}")
+    private final long REFRESH_TOKEN_EXPIRY;
+
+    public CookieServiceImpl(String cookieName, long refreshTokenExpiry) {
+        this.COOKIE_NAME = cookieName;
+        this.REFRESH_TOKEN_EXPIRY = refreshTokenExpiry;
+    }
 }
