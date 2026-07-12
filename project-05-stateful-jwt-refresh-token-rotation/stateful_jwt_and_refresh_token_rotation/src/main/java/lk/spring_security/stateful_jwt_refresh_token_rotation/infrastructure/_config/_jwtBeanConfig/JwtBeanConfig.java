b1153cbadd764c2fbd712d40bb0aeffffc0e5b14
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-12 20:23:28 +0530

    feat: initiate jwt service as bean through configuration class

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_jwtBeanConfig/JwtBeanConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_jwtBeanConfig/JwtBeanConfig.java
@@ -1,4 +1,18 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._jwtBeanConfig;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.JwtServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
 public class JwtBeanConfig {
+    @Bean
+    public TokenService tokenService(
+            SecretKey secretKey
+    ) {
+        return new JwtServiceImpl(secretKey);
+    }
 }
