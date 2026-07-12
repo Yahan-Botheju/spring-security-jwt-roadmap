ad36455866f1f4efe3cf4fb7d674a1a863dd584a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-12 22:10:46 +0530

    feat: initiate token builder method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/JwtServiceImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/JwtServiceImpl.java
@@ -1,8 +1,12 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security;
 
+import io.jsonwebtoken.Jwts;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
+import lombok.Data;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 
 public class JwtServiceImpl implements TokenService {
 
@@ -21,4 +25,21 @@ public class JwtServiceImpl implements TokenService {
         this.accessTokenExpirationMs = accessTokenExpirationMs;
         this.refreshTokenExpirationMs = refreshTokenExpirationMs;
     }
+
+     /* __JWT HELPER METHODS__ */
+
+    //common token builder method
+     private String buildToken(Map<String, Object> extraClaims, String subject, long expirationMs) {
+        return  Jwts.builder()
+                .claims(extraClaims)
+                .subject(subject)
+                .issuedAt(new Data(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(secretKey)
+                .compact();
+     }
+
+    /* __VALIDATION METHODS__ */
+
+
 }
