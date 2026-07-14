a0bb0292f6726c5881324fd719f5de5d1f74a04c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-14 21:13:56 +0530

    feat: create password encoder

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/_config/ApplicationConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/_config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security._config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
@@ -11,5 +14,11 @@ public class ApplicationConfig {
 
     public ApplicationConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    //initiate password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
