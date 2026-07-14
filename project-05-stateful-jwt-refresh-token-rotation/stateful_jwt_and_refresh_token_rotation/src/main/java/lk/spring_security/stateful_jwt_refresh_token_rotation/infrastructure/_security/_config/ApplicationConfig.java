c5c5116ae802987585beca83ac12e874b9746096
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-14 21:12:23 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/_config/ApplicationConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/_config/ApplicationConfig.java
@@ -1,4 +1,15 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security._config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
 public class ApplicationConfig {
+
+    //inject required dependencies
+    private final UserDetailsService userDetailsService;
+
+    public ApplicationConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 }
