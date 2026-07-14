31f21b140f955d42526e6d0e9b58be07c2b052bb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-14 20:28:18 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,26 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.filter;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.CookieService;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //inject required dependencies
+    private final CookieService cookieService;
+    private final TokenService tokenService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(
+            CookieService cookieService,
+            TokenService tokenService,
+            UserDetailsService userDetailsService
+    ) {
+        this.cookieService = cookieService;
+        this.tokenService = tokenService;
+        this.userDetailsService = userDetailsService;
+    }
 }
