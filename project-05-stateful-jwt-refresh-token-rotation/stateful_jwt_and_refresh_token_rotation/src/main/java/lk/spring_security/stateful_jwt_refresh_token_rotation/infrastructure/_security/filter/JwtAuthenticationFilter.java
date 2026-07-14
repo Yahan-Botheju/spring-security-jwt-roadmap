1dfafd25b3b873c2515aa10156cc23585b28b6a0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-14 20:26:42 +0530

    feat: extends with spring once request filter interface

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.filter;
 
-public class JwtAuthenticationFilter {
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 }
