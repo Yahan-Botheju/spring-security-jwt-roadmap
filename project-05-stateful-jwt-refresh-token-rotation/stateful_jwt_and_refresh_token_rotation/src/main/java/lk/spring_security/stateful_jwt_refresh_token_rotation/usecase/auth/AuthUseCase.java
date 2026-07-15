c565171ef678202a722b7bb7456a63d6a9e0a322
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-15 22:43:17 +0530

    feat: initiate register user method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/AuthUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/AuthUseCase.java
@@ -1,4 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.auth;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
+
 public interface AuthUseCase {
+
+    //register user
+    void registerUser(User user);
 }
