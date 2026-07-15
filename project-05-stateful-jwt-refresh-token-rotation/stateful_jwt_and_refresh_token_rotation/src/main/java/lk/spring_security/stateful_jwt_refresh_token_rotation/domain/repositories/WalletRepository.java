4f5407be0aebd44ebf0880923605fcb6d1f2fcda
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-15 22:48:45 +0530

    feat: create save wallet method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/WalletRepository.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/WalletRepository.java
@@ -1,4 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
+
 public interface WalletRepository {
+
+    //save wallet
+    Wallet saveWallet(Wallet wallet);
 }
