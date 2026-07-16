4313f35a2b9930f96776f21fdad54f3ba6b4f6d2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-16 10:59:41 +0530

    fix: change the return type as record

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/AuthUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/AuthUseCase.java
@@ -1,9 +1,13 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.auth;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
 
 public interface AuthUseCase {
 
     //register user
     void registerUser(User user);
+
+    //login user
+    AuthResult loginUser(String email, String password, HttpServletResponse httpServletResponse);
 }
