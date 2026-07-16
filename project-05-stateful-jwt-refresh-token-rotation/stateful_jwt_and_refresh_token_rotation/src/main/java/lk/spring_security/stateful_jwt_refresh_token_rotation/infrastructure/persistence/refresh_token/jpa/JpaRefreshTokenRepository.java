83352173c7f55e54989b011d20d9f7d2edc63966
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-16 12:08:56 +0530

    feat: intiate custom query for get token by token

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/refresh_token/jpa/JpaRefreshTokenRepository.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/persistence/refresh_token/jpa/JpaRefreshTokenRepository.java
@@ -3,5 +3,9 @@ package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.pe
 import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure.persistence.refresh_token.entities.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaRefreshTokenRepository extends JpaRepository<RefreshTokenEntity,Long> {
+
+    Optional<RefreshTokenEntity> findByToken(String token);
 }
