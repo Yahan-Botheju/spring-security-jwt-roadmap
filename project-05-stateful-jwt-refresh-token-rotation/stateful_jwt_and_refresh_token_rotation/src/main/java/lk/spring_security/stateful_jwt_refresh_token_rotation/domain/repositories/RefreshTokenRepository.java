18dda57499765aa0c84c85d238e190f18046c975
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-16 11:52:31 +0530

    feat: initiate token find method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/RefreshTokenRepository.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/RefreshTokenRepository.java
@@ -2,8 +2,15 @@ package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositori
 
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository {
+
+    //find token
+    Optional<RefreshToken> findByToken(String token);
 
     //save token
     RefreshToken saveRefreshToken(RefreshToken refreshToken);
+
+
 }
