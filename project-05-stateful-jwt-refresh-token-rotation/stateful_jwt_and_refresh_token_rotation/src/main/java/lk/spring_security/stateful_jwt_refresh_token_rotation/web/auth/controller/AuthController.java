616660ce58a9b265c04ba285a530e03b0bb60be0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-17 21:54:12 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/auth/controller/AuthController.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/auth/controller/AuthController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.auth.controller;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.auth.AuthUseCase;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.web.auth.webMapper.AuthWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    //inject required dependencies
+    private final AuthUseCase authUseCase;
+    private final AuthWebMapper authWebMapper;
+
+    public AuthController(
+            AuthUseCase authUseCase,
+            AuthWebMapper authWebMapper
+    ) {
+        this.authUseCase = authUseCase;
+        this.authWebMapper = authWebMapper;
+    }
 }
