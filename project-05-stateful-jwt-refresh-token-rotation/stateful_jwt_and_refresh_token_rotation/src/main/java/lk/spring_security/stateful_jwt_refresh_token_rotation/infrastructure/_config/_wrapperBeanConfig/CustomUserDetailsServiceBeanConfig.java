d917e0aa679a13a788088e8f65aa20c90481fe05
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-17 21:48:17 +0530

    fix: return custom user details service

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_wrapperBeanConfig/CustomUserDetailsServiceBeanConfig.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_config/_wrapperBeanConfig/CustomUserDetailsServiceBeanConfig.java
@@ -1,6 +1,7 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._config._wrapperBeanConfig;
 
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.user_spring_wrapper.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,6 @@ public class CustomUserDetailsServiceBeanConfig {
     public UserDetailsService userDetailsService(
             UserRepository userRepository
     ) {
-        return new UserDetailsService(userRepository);
+        return new CustomUserDetailsService(userRepository);
     }
 }
