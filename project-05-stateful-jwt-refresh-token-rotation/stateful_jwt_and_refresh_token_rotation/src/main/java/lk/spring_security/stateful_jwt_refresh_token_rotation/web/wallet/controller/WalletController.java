27f55aed0bae5c841fb628114862f81a4e255a8d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-18 21:58:30 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/controller/WalletController.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/controller/WalletController.java
@@ -1,4 +1,23 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.controller;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.WalletUseCase;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.webMapper.WalletWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/wallet")
 public class WalletController {
+
+    //inject required dependencies
+    private final WalletUseCase walletUseCase;
+    private final WalletWebMapper walletWebMapper;
+
+    public WalletController(
+            WalletUseCase walletUseCase,
+            WalletWebMapper walletWebMapper
+    ) {
+        this.walletUseCase = walletUseCase;
+        this.walletWebMapper = walletWebMapper;
+    }
 }
