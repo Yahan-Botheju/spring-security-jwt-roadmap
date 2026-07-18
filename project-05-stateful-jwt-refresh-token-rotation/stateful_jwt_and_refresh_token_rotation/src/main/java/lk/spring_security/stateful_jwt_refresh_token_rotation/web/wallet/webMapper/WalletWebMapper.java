c56cf837d215b77c64c1df85a98fed8f8b1bc5c6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-18 22:07:29 +0530

    feat: create wallet web mapper

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/webMapper/WalletWebMapper.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/webMapper/WalletWebMapper.java
@@ -1,4 +1,13 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.webMapper;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs.WalletResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface WalletWebMapper {
+    //domain model to responseDTO
+    @Mapping(source = "user.email", target = "email")
+    WalletResponseDTO toResponseDTO(Wallet wallet);
 }
