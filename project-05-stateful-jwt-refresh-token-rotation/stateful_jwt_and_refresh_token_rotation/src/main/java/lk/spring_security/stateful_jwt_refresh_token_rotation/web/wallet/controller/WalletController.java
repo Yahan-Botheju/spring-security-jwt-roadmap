584345ce2c0fcde28552ce3d290ce9a37bc13b88
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-18 22:01:31 +0530

    feat: get authenticated user email

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/controller/WalletController.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/controller/WalletController.java
@@ -1,8 +1,14 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.controller;
 
 import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.WalletUseCase;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs.WalletResponseDTO;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.webMapper.WalletWebMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +25,16 @@ public class WalletController {
     ) {
         this.walletUseCase = walletUseCase;
         this.walletWebMapper = walletWebMapper;
+    }
+
+    //get wallet balance
+    @GetMapping("/balance")
+    public ResponseEntity<WalletResponseDTO> getWalletBalance(
+            @AuthenticationPrincipal UserDetails userDetails
+            ){
+        //get user email
+        String currentUserEmail = userDetails.getUsername();
+
+
     }
 }
