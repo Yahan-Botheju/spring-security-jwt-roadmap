ac95467405fdb84ec6146e716ee33c10b28e5428
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-18 22:04:48 +0530

    feat: initiate user find by email method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/WalletRepository.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/WalletRepository.java
@@ -2,8 +2,14 @@ package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositori
 
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface WalletRepository {
 
     //save wallet
     Wallet saveWallet(Wallet wallet);
+
+    //wallet find by email
+    Optional<Wallet> findByUserEmail(String email);
 }
