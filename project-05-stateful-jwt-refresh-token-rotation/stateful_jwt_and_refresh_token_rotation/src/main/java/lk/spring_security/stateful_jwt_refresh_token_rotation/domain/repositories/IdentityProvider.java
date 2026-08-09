0cf78043ae84cc4f8c84341a6cd472b88fd5e2e8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-09 13:33:49 +0530

    feat: initiate user authentication method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/IdentityProvider.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/IdentityProvider.java
@@ -1,9 +1,12 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.auth.AuthResult;
+
 public interface IdentityProvider {
 
     //password encoder
     String passwordEncoder(String rawPassword);
 
-
+    //authenticate user through auth manager
+    AuthResult authenticate(String username, String password);
 }
