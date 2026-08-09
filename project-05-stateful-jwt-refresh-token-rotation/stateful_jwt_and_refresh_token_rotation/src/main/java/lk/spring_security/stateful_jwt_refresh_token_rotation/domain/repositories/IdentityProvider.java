7a50e2b76917181216cc755fcc727415a5aa2d2a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-09 13:22:05 +0530

    ref: initiate password encoder method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/IdentityProvider.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/repositories/IdentityProvider.java
@@ -1,4 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories;
 
 public interface IdentityProvider {
+
+    //password encoder
+    String passwordEncoder(String rawPassword);
+
+
 }
