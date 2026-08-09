6c177dbec347c2559518b7f20a8cb1f10b268dac
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-09 12:44:06 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/identity_provider/IdentityProviderImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/identity_provider/IdentityProviderImpl.java
@@ -1,4 +1,20 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.identity_provider;
 
-public class IdentityProviderImpl {
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.IdentityProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class IdentityProviderImpl implements IdentityProvider {
+
+    //inject required dependencies
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public IdentityProviderImpl(
+            PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager
+    ) {
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
 }
