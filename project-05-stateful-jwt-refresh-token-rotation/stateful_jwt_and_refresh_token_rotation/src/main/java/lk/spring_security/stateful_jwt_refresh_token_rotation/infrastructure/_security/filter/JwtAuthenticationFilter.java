561d94598735403a3b6539207fc46526ce080b39
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-09 14:32:13 +0530

    ref: remove annotation and remove unused classes

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/filter/JwtAuthenticationFilter.java
@@ -4,32 +4,26 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.CookieService;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     //inject required dependencies
-    private final CookieService cookieService;
     private final TokenService tokenService;
     private final UserDetailsService userDetailsService;
 
     public JwtAuthenticationFilter(
-            CookieService cookieService,
             TokenService tokenService,
             UserDetailsService userDetailsService
     ) {
-        this.cookieService = cookieService;
         this.tokenService = tokenService;
         this.userDetailsService = userDetailsService;
     }
