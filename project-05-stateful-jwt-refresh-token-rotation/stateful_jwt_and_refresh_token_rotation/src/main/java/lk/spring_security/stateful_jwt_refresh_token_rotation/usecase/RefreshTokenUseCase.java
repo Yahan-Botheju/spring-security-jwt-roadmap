0f77eea4f5a905afa6ee33ec7ac51062e974362a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 20:41:16 +0530

    ref: change param

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RefreshTokenUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RefreshTokenUseCase.java
@@ -1,10 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
-import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RefreshTokenCommand;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RefreshTokenResult;
 
 public interface RefreshTokenUseCase {
 
     //register user
-    RefreshTokenResult registerUser(RefreshTokenCommand refreshTokenCommand);
+    RefreshTokenResult refreshToken(String refreshToken);
 }
