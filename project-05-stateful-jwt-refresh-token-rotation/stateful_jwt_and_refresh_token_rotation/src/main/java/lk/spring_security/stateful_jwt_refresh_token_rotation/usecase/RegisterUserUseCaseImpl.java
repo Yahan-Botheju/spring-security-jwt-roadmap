5eb32bb571d60eb154a3cecef1adeee6d3194241
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 21:05:57 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RegisterUserUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RegisterUserUseCaseImpl.java
@@ -1,4 +1,23 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.IdentityProvider;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+
 public class RegisterUserUseCaseImpl {
+
+    //inject required dependencies
+    private final UserRepository userRepository;
+    private final IdentityProvider identityProvider;
+    private final WalletRepository walletRepository;
+
+    public RegisterUserUseCaseImpl(
+            UserRepository userRepository,
+            IdentityProvider identityProvider,
+            WalletRepository walletRepository
+    ) {
+        this.userRepository = userRepository;
+        this.identityProvider = identityProvider;
+        this.walletRepository = walletRepository;
+    }
 }
