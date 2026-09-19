19d0804c7caee5eb317f176c84960f86aebf9683
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 21:34:31 +0530

    feat: create separate class for register user

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RegisterUserUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RegisterUserUseCaseImpl.java
@@ -1,10 +1,14 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.IdentityProvider;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RegisterUseCommand;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RegisterUserResult;
 
-public class RegisterUserUseCaseImpl {
+public class RegisterUserUseCaseImpl implements RegisterUserUseCase {
 
     //inject required dependencies
     private final UserRepository userRepository;
@@ -19,5 +23,39 @@ public class RegisterUserUseCaseImpl {
         this.userRepository = userRepository;
         this.identityProvider = identityProvider;
         this.walletRepository = walletRepository;
+    }
+
+    @Override
+    public RegisterUserResult registerUser(RegisterUseCommand registerUseCommand){
+
+        //check user existence by email
+        if(userRepository.findByEmail(registerUseCommand.email()).isPresent()){
+            throw new IllegalStateException("User already exists");
+        }
+
+        //create new user model
+        User newUser = User.createNewUser(
+                registerUseCommand.email(),
+                identityProvider.passwordEncoder(registerUseCommand.password())
+        );
+
+        //save user
+        User savedUser = userRepository.registerUser(newUser);
+
+        //create default wallet for new user
+        Wallet newWallet = Wallet.createNewWallet(
+                savedUser
+        );
+
+        //save wallet
+        walletRepository.saveWallet(newWallet);
+
+        return new RegisterUserResult(
+                savedUser.getUserId(),
+                savedUser.getEmail(),
+                savedUser.getRole().name(),
+                newWallet.getWalletId(),
+                newWallet.getWalletBalance()
+        );
     }
 }
