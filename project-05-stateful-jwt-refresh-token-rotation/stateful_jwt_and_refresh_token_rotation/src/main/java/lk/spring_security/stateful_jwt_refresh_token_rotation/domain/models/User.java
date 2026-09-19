48163d64d080c6e9e610a44358541bfb4baec4ad
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 21:16:31 +0530

    feat: create factory method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/User.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/User.java
@@ -1,13 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
 @Builder
 public class User {
     private Long userId;
@@ -16,9 +11,21 @@ public class User {
 
     private Role role;
 
+
+    /* __FACTORY_METHOD__ */
+    public static User createNewUser(
+            Long userId,
+            String email,
+            String password,
+            Role role
+    ) {
+        return new User(userId, email, password, role);
+    }
+
+
     /* __DOMAIN_LOGIC__ */
-    public void setDefaultRole(){
-        if(role != null){
+    public void setDefaultRole() {
+        if (role != null) {
             throw new IllegalStateException("There is already a role set");
         }
         this.role = Role.USER;
