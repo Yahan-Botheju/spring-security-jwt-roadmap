668b622ffa1142b3d55c60ccd12ccaecb3a8a4d4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 18:53:21 +0530

    feat: initiate method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RefreshTokenUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/RefreshTokenUseCase.java
@@ -1,4 +1,10 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RefreshTokenCommand;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.RefreshTokenResult;
+
 public interface RefreshTokenUseCase {
+
+    //register user
+    RefreshTokenResult registerUser(RefreshTokenCommand refreshTokenCommand);
 }
