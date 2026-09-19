675483ed4dff51378e157e16f5f753038fc2046f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-19 20:42:53 +0530

    ref: update with new field

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/records/RefreshTokenResult.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/records/RefreshTokenResult.java
@@ -1,15 +1,10 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records;
 
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
-
-import java.time.Instant;
 
 public record RefreshTokenResult(
-        Long tokenId,
-        String token,
-        Instant expiryDate,
-        boolean isUsed,
-        boolean isRevoked,
-        User user
+        String newAccessToken,
+        String newRefreshToken,
+        String email,
+        String role
 ) {
 }
