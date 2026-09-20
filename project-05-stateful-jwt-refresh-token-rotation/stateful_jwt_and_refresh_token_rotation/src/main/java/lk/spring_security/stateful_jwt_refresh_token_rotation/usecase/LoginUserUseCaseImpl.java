35dbf8965dbbacb4b9926f266d443c006e36ba18
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-20 20:03:45 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LoginUserUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LoginUserUseCaseImpl.java
@@ -1,11 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.CookieService;
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.IdentityProvider;
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.TokenService;
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.UserRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.User;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.*;
 
-public class LoginUserUseCaseImpl implements LoginUserUseCase {
+public class LoginUserUseCaseImpl extends GenerateRefreshTokenSupport implements LoginUserUseCase {
 
     //inject required dependencies
     private final UserRepository userRepository;
@@ -17,11 +15,27 @@ public class LoginUserUseCaseImpl implements LoginUserUseCase {
             UserRepository userRepository,
             TokenService tokenService,
             IdentityProvider identityProvider,
-            CookieService cookieService
+            CookieService cookieService,
+            RefreshTokenRepository refreshTokenRepository
     ) {
+        super(refreshTokenRepository);
         this.userRepository = userRepository;
         this.tokenService = tokenService;
         this.identityProvider = identityProvider;
         this.cookieService = cookieService;
+    }
+
+    public void loginUser(String email, String password){
+
+        //check email password correctness through auth provider
+        identityProvider.authenticate(email, password);
+
+        //get user from db
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+
+        //generate tokens
+        String accessToken = tokenService.generateAccessToken(user);
+        String refreshToken = tokenService.generateRefreshToken(user);
     }
 }
