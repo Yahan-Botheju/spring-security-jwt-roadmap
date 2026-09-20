53e7e76514bd6df0d0acffb9727d85fa8a07fcae
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-20 21:38:04 +0530

    feat: create factory method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/Wallet.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/domain/models/Wallet.java
@@ -1,17 +1,25 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter
 public class Wallet {
     private Long walletId;
     private Double walletBalance;
 
     private User user;
+
+    public Wallet(Long walletId, Double walletBalance, User user) {
+        this.walletId = walletId;
+        this.walletBalance = walletBalance;
+        this.user = user;
+    }
+
+    /* __FACTORY_METHOD__ */
+
+    public static Wallet createNewWallet(
+            User user
+    ){
+        return new Wallet(null,1000.0, user);
+    }
 }
