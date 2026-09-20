26e618b7b3649a54447152933f96f8e038a9187c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-20 20:56:45 +0530

    feat: logout user functionality

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LogoutUserUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LogoutUserUseCaseImpl.java
@@ -1,6 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.RefreshToken;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.LogoutUserCommand;
 
 public class LogoutUserUseCaseImpl implements LogoutUserUseCase {
 
@@ -9,5 +11,21 @@ public class LogoutUserUseCaseImpl implements LogoutUserUseCase {
 
     public LogoutUserUseCaseImpl(RefreshTokenRepository refreshTokenRepository) {
         this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    //create logout user
+    @Override
+    public void logout(LogoutUserCommand logoutUserCommand) {
+        //toke validation
+        if(logoutUserCommand.refreshToken() == null || logoutUserCommand.refreshToken().isBlank()){
+            return;
+        }
+        //get token
+        RefreshToken token = refreshTokenRepository.findByToken(logoutUserCommand.refreshToken())
+                .orElseThrow(() ->  new RuntimeException("Refresh token not found"));
+        //use domain model
+        token.markAsUsed();
+        //save as use one
+        refreshTokenRepository.saveRefreshToken(token);
     }
 }
