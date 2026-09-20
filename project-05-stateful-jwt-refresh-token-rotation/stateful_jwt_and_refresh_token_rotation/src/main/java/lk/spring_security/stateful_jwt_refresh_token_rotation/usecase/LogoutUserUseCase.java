8b2dd0f43f9e88cc4dc965a2282dba63a24fa6d5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-20 20:56:26 +0530

    feat: initiate method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LogoutUserUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/LogoutUserUseCase.java
@@ -1,4 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records.LogoutUserCommand;
+
 public interface LogoutUserUseCase {
+    //logout user
+    void logout(LogoutUserCommand logoutUserCommand);
 }
