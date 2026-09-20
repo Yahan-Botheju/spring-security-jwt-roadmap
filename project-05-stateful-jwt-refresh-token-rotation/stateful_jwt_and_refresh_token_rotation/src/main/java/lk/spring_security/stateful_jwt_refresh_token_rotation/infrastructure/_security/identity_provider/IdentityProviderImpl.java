67338a944bb2ec13af4b78dfcfb927133974c0df
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-20 21:38:43 +0530

    ref: upgrade

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/identity_provider/IdentityProviderImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/infrastructure/_security/identity_provider/IdentityProviderImpl.java
@@ -1,11 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.identity_provider;
 
-import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.records.AuthenticatedUser;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.IdentityProvider;
-import lk.spring_security.stateful_jwt_refresh_token_rotation.infrastructure._security.user_spring_wrapper.CustomUserDetails;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class IdentityProviderImpl implements IdentityProvider {
@@ -30,18 +27,7 @@ public class IdentityProviderImpl implements IdentityProvider {
 
     //authenticate user through auth manager
     @Override
-    public AuthenticatedUser authenticate(String username, String password){
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(username, password)
-        );
-
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        return new AuthenticatedUser(
-                null,
-                customUserDetails.getUsername(),
-                customUserDetails.getUser().getRole().name()
-        );
+    public void authenticate(String username, String password){
+       authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 }
