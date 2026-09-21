eeae345665039b2fb3e184d75e180108ed911c75
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 20:12:27 +0530

    feat: inject required dependencies

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/DepositMoneyUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/DepositMoneyUseCaseImpl.java
@@ -1,4 +1,14 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet;
 
-public class DepositeMoneyUseCaseImpl {
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+
+public class DepositMoneyUseCaseImpl implements DepositMoneyUseCase {
+
+    //inject required dependencies
+    private final WalletRepository walletRepository;
+
+    public DepositMoneyUseCaseImpl(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
 }
