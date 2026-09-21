8c95902929db2853b99c8fcf2b8a2b2629feea02
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 19:51:34 +0530

    feat: create new response for get wallet balance

---
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/WalletBalanceResponseDTO.java
@@ -0,0 +1,13 @@
+package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WalletBalanceResponseDTO {
+    private String email;
+    private Double walletBalance;
+}
