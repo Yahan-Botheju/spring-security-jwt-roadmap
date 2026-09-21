d06f88400f3bca05ed4e2bac254b8b7c958c538e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 19:08:47 +0530

    fix: remove private method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/RefreshTokenUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/RefreshTokenUseCaseImpl.java
@@ -23,13 +23,6 @@ public class RefreshTokenUseCaseImpl extends  GenerateRefreshTokenSupport implem
         this.tokenService = tokenService;
     }
 
-    /*  __HELPER_METHODS__  */
-
-    //generate new refresh token and save in db
-    private void generateNewRefreshToken(User user, String refreshToken){
-        RefreshToken newRefreshToken = RefreshToken.createNewRefreshToken(user, refreshToken);
-        refreshTokenRepository.saveRefreshToken(newRefreshToken);
-    }
 
     /*  __PUBLIC_METHODS__  */
 
@@ -59,7 +52,7 @@ public class RefreshTokenUseCaseImpl extends  GenerateRefreshTokenSupport implem
         String newRefreshToken = tokenService.generateRefreshToken(user);
 
         //use private method
-        generateNewRefreshToken(user, newRefreshToken);
+        generateRefreshToken(user, newRefreshToken);
 
 
         return new RefreshTokenResult(
