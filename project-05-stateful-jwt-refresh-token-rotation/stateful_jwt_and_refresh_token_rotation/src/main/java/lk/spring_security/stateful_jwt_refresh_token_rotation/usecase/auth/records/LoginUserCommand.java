15f1a5e0f4fd5b7eb7ec77c9876898cdd8b825ef
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 19:44:08 +0530

    ref: move into folder

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/records/LoginUserCommand.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/auth/records/LoginUserCommand.java
@@ -1,4 +1,4 @@
-package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.records;
+package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.auth.records;
 
 
 public record LoginUserCommand(
