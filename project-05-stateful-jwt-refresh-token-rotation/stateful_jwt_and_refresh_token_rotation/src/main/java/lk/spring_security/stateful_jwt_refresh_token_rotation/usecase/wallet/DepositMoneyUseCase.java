970d5f215ed209331bca055cbe957bd93512184f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 20:35:06 +0530

    feat: initiate method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/DepositMoneyUseCase.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/DepositMoneyUseCase.java
@@ -1,4 +1,10 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet;
 
-public interface DepositeMoneyUseCase {
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.DepositCommand;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.DepositResult;
+
+public interface DepositMoneyUseCase {
+
+    //get deposit amount
+    DepositResult depositMoney(DepositCommand depositCommand);
 }
