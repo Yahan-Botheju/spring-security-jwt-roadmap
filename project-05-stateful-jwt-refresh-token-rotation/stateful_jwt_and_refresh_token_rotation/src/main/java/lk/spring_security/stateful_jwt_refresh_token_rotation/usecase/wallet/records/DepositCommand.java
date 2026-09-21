519cd5b8b757c07d52601a93404e6b5c8a9bf42c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 20:10:17 +0530

    feat: create command class for deposit money

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/records/DepositCommand.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/records/DepositCommand.java
@@ -1,4 +1,8 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records;
 
-public record DepositeCommand() {
+public record DepositCommand(
+        String email,
+        double amount
+
+) {
 }
