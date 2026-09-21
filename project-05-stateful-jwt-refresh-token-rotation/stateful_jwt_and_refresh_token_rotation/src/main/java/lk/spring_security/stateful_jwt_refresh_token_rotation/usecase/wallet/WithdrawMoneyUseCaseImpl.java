f436c99496912b58961ae86577b10d118bd97cd6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 20:51:15 +0530

    feat: create funcationality of withdraw money method

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/WithdrawMoneyUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/WithdrawMoneyUseCaseImpl.java
@@ -1,6 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.WithdrawCommand;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.WithdrawResult;
 
 public class WithdrawMoneyUseCaseImpl implements WithdrawMoneyUseCase {
 
@@ -9,5 +12,29 @@ public class WithdrawMoneyUseCaseImpl implements WithdrawMoneyUseCase {
 
     public WithdrawMoneyUseCaseImpl(WalletRepository walletRepository) {
         this.walletRepository = walletRepository;
+    }
+
+    //withdraw money
+    @Override
+    public WithdrawResult withdrawMoney(WithdrawCommand withdrawCommand) {
+        //validate incoming values
+        if(withdrawCommand.email().isBlank() || withdrawCommand.amount() <= 0){
+            throw new IllegalArgumentException("Cannot withdraw amount less or zero amount.");
+        }
+
+        //find user related to wallet
+        Wallet userWallet = walletRepository.findByUserEmail(withdrawCommand.email())
+                .orElseThrow(() ->  new RuntimeException("users' wallet not found" + "," + withdrawCommand.email()));
+
+        //calculate new balance and update
+        Double newBalance = userWallet.getWalletBalance() - withdrawCommand.amount();
+        userWallet.depositMoney(newBalance);
+
+        walletRepository.saveWallet(userWallet);
+
+        return new WithdrawResult(
+                userWallet.getUser().getEmail(),
+                userWallet.getWalletBalance()
+        );
     }
 }
