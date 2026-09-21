0edbeae5b490e88758174d08162e2f3700bb5774
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-21 20:01:14 +0530

    feat: create wallet balance functionality

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/WalletBalanceUseCaseImpl.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/usecase/wallet/WalletBalanceUseCaseImpl.java
@@ -1,6 +1,9 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet;
 
+import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.models.Wallet;
 import lk.spring_security.stateful_jwt_refresh_token_rotation.domain.repositories.WalletRepository;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.WalletBalanceCommand;
+import lk.spring_security.stateful_jwt_refresh_token_rotation.usecase.wallet.records.WalletBalanceResult;
 
 public class WalletBalanceUseCaseImpl implements WalletBalanceUseCase {
 
@@ -9,5 +12,20 @@ public class WalletBalanceUseCaseImpl implements WalletBalanceUseCase {
 
     public WalletBalanceUseCaseImpl(WalletRepository walletRepository) {
         this.walletRepository = walletRepository;
+    }
+
+    //get wallet balance
+    @Override
+    public WalletBalanceResult getWalletBalance(WalletBalanceCommand walletBalanceCommand) {
+
+         Wallet getWallet =  walletRepository.findByUserEmail(walletBalanceCommand.email())
+                .orElseThrow(() ->  new RuntimeException("wallet not found"));
+
+
+         return new  WalletBalanceResult(
+                 getWallet.getUser().getEmail(),
+                 getWallet.getWalletBalance()
+         );
+
     }
 }
