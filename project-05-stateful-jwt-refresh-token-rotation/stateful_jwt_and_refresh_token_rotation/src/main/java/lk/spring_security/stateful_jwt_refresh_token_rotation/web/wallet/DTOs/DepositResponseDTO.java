04fa292c3737aa8ece4616d34bbbb3056ffa1cd2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-22 19:03:59 +0530

    feat: create deposit response dto

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/DepositResponseDTO.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/DepositResponseDTO.java
@@ -1,4 +1,13 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs;
 
-public class DepositeResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepositResponseDTO {
+    private String email;
+    private Double walletBalance;
 }
