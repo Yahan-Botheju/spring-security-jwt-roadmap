b9c0a67ab8e42c571ec906503fe2d929ee602c16
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-22 19:03:15 +0530

    feat: create deposit request dto

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/DepositRequestDTO.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/DepositRequestDTO.java
@@ -1,4 +1,19 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs;
 
-public class DepositeRequestDTO {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepositRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    @NotNull(message = "Amount cannot be empty")
+    private double amount;
 }
