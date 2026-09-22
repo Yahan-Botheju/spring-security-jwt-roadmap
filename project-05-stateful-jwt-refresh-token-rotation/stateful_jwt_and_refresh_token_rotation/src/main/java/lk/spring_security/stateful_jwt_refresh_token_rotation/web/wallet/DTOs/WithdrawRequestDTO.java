3804e8e99089d5a40cba6eb458f43c144296f059
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-22 19:05:03 +0530

    feat: create with draw request dto

--- project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/WithdrawRequestDTO.java
+++ project-05-stateful-jwt-refresh-token-rotation/stateful_jwt_and_refresh_token_rotation/src/main/java/lk/spring_security/stateful_jwt_refresh_token_rotation/web/wallet/DTOs/WithdrawRequestDTO.java
@@ -1,4 +1,19 @@
 package lk.spring_security.stateful_jwt_refresh_token_rotation.web.wallet.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WithdrawRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    @NotNull(message = "Amount cannot be empty")
+    private Double amount;
 }
