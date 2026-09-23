a6113f3c6678aff5e9ce68949aad46ad1633ed95
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-23 21:48:43 +0530

    feat: inject required dependencies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/RegisterUseCaseImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/RegisterUseCaseImpl.java
@@ -1,13 +1,44 @@
 package lk.spring_security.refresh_token.usecase.auth;
 
+import lk.spring_security.refresh_token.domain.enums.Role;
+import lk.spring_security.refresh_token.domain.models.User;
 import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+import lk.spring_security.refresh_token.usecase.auth.records.RegisterCommand;
+import lk.spring_security.refresh_token.usecase.auth.records.RegisterResult;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class RegisterUseCaseImpl implements RegisterUseCase {
 
     //inject required dependencies
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegisterUseCaseImpl(UserRepository userRepository) {
+    public RegisterUseCaseImpl(UserRepository userRepository,  PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public RegisterResult register(RegisterCommand registerCommand) {
+
+        if(registerCommand.email() == null || registerCommand.password() == null) {
+            throw new IllegalArgumentException("Fields email and password are mandatory");
+        }
+
+        if(userRepository.findByEmail(registerCommand.email()).isPresent()){
+            throw new IllegalArgumentException("User with this email already exists");
+        }
+
+        User newUser = User.createUser(
+                e
+        )
+
+
+        //hash the password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        //set default role as USER
+        user.setRole(Role.USER);
+
+        return userRepository.registerUser(user);
     }
 }
