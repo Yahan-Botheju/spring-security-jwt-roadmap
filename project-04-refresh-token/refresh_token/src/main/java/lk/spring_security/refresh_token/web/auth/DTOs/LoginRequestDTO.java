f71a0fc3f4f1628d53744a8927b2b23339b97390
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-24 20:56:48 +0530

    feat: create login request dto

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/LoginRequestDTO.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/LoginRequestDTO.java
@@ -1,4 +1,18 @@
 package lk.spring_security.refresh_token.web.auth.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
 }
