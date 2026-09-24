a4e36b4a7091ea16051e629918b3f9534ca43aa4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-24 21:01:37 +0530

    feat: inject required dependencies

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/LoginUseCaseImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/LoginUseCaseImpl.java
@@ -1,4 +1,27 @@
 package lk.spring_security.refresh_token.usecase.auth;
 
-public class LoginUseCaseImpl {
+import lk.spring_security.refresh_token.domain.repositories.CookieService;
+import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
+import lk.spring_security.refresh_token.domain.repositories.TokenService;
+import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+
+public class LoginUseCaseImpl implements LoginUseCase {
+
+    //inject required dependencies
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final CookieService cookieService;
+
+    public LoginUseCaseImpl(
+            UserRepository userRepository,
+            TokenService tokenService,
+            RefreshTokenRepository refreshTokenRepository,
+            CookieService cookieService
+    ) {
+        this.userRepository = userRepository;
+        this.tokenService = tokenService;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.cookieService = cookieService;
+    }
 }
