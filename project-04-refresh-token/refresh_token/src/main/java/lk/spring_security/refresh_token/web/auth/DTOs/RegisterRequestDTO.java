682aba18e9efc29fbe17176c76d261fc868f0b49
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-24 20:13:48 +0530

    feat: create register request dto

---
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/web/auth/DTOs/RegisterRequestDTO.java
@@ -0,0 +1,18 @@
+package lk.spring_security.refresh_token.web.auth.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDTO {
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+}
