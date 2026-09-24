d5e3b95203713d32108d98f1602a372fffd22cc4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-24 20:56:30 +0530

    feat: create login resutl

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/records/LoginResult.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/records/LoginResult.java
@@ -1,4 +1,15 @@
 package lk.spring_security.refresh_token.usecase.auth.records;
 
-public record LoginResult() {
+import lk.spring_security.refresh_token.domain.enums.Role;
+
+import java.time.LocalDateTime;
+
+public record LoginResult(
+        Long userId,
+        String email,
+        Role role,
+        String refreshToken,
+        String accessToken,
+        LocalDateTime expiryDate
+) {
 }
