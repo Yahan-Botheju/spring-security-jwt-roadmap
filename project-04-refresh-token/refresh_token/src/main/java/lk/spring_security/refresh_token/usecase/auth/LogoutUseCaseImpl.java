0885c0bcb640a081365cf8d341d52227b5318146
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-25 20:51:50 +0530

    feat: create logout usecase functionality

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/LogoutUseCaseImpl.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/LogoutUseCaseImpl.java
@@ -2,6 +2,8 @@ package lk.spring_security.refresh_token.usecase.auth;
 
 import lk.spring_security.refresh_token.domain.repositories.RefreshTokenRepository;
 import lk.spring_security.refresh_token.domain.repositories.UserRepository;
+import lk.spring_security.refresh_token.usecase.auth.records.LogoutCommand;
+import lk.spring_security.refresh_token.usecase.auth.records.LogoutResult;
 
 public class LogoutUseCaseImpl implements LogoutUseCase {
 
@@ -15,5 +17,24 @@ public class LogoutUseCaseImpl implements LogoutUseCase {
     ) {
         this.userRepository = userRepository;
         this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    //logout
+    @Override
+    public LogoutResult logout(LogoutCommand logoutCommand){
+
+        String tokenStr = logoutCommand.refreshToken();
+
+        //token availability
+        if(tokenStr != null && !tokenStr.isBlank()){
+            //delete token if available
+            refreshTokenRepository.findByToken(tokenStr)
+                    .ifPresent(refreshToken ->
+                            refreshTokenRepository.deleteByToken(tokenStr));
+
+        }
+        return new LogoutResult(
+                "User Logout Successful!!"
+        );
     }
 }
