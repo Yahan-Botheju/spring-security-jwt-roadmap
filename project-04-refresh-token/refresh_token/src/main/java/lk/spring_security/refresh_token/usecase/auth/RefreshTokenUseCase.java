2d238a6c99c87798f8b1813cb7670595e5f0b305
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-25 22:01:11 +0530

    feat: initiate method

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/RefreshTokenUseCase.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/auth/RefreshTokenUseCase.java
@@ -1,4 +1,9 @@
 package lk.spring_security.refresh_token.usecase.auth;
 
+import lk.spring_security.refresh_token.usecase.auth.records.RefreshTokenCommand;
+import lk.spring_security.refresh_token.usecase.auth.records.RefreshTokenResult;
+
 public interface RefreshTokenUseCase {
+    //refresh token
+    RefreshTokenResult refreshToken(RefreshTokenCommand refreshTokenCommand);
 }
