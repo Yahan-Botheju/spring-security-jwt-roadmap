ec5015d411cb50f24f9de465105a1e4984fe84df
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-26 21:15:47 +0530

    feat: create update product result

--- project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/product/records/UpdateProductResult.java
+++ project-04-refresh-token/refresh_token/src/main/java/lk/spring_security/refresh_token/usecase/product/records/UpdateProductResult.java
@@ -1,4 +1,8 @@
 package lk.spring_security.refresh_token.usecase.product.records;
 
-public record UpdateProductResult() {
+public record UpdateProductResult(
+        Long  productId,
+        String productName,
+        Double productPrice
+) {
 }
